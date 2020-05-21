404e2b173a8827110342bbb8a45e47696d11c13b
Kristina <cristina.chiticari@gmail.com>
2020-05-21 17:27:21 +0000

    40

---
+++ exercises/40-get-First-Element/README.md
@@ -0,0 +1,12 @@
+# `40` Get First Element
+
+Write a function called "getFirstElement".
+
+Given an array, "getFirstElement" returns the first element of the given array.
+Notes:* If the given array has a length of 0, it should return undefined.
+
+
+```js
+var output = getFirstElement([1, 2, 3, 4, 5]);
+console.log(output); // --> 1
+```
--- exercises/40-get-First-Element/tests.js
+++ exercises/40-get-First-Element/tests.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 let _buffer = "";
 global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
 
-test("Function getNthElement should exist", function(){
+test("Function getFirstElement should exist", function(){
   const file = rewire("./app.js");
-  const getNthElement  = file.__get__('getNthElement');
-  expect(getNthElement).toBeTruthy();
+  const getFirstElement  = file.__get__('getFirstElement');
+  expect(getFirstElement).toBeTruthy();
 });
 
 
@@ -19,13 +19,13 @@ describe('All the javascript should match', function () {
     afterEach(() => { jest.resetModules(); });
 
 
-    it('console.log() function getNthElement should return given integer of 3', function () {
+    it('console.log() function getFirstElement should return given integer of 1', function () {
         const file = require("./app.js");
 
  function getNthElement(arr, n) {
   return arr[n];
 }
-expect(console.log.mock.calls.length).toBe(3);
+expect(console.log.mock.calls.length).toBe(1);
     // expect(_buffer).toEqual(mytest);
     });
 });

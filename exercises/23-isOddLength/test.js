3889f33e0e5549ba73a9cfb065a4d3eead7144dd
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 15:30:26 +0000

    Tested and Updated #23

--- exercises/23-isOddLength/test.js
+++ exercises/23-isOddLength/test.js
@@ -1,5 +1,10 @@
 const rewire = require ("rewire");
 
+test('Function isOddLength must exist', () => {
+    const isOddLength = rewire("./app.js").__get__("isOddLength");
+    expect(isOddLength).not.toBe(undefined);
+});
+
 test ('function should return whether the length of the given word is odd.', () => {
     const isOddLength = rewire ('./app.js').__get__("isOddLength");
 

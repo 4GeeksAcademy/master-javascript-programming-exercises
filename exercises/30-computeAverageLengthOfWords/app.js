f7a9d0ce177bf60951ded944542e1d867e4fc70d
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 16:00:57 +0000

    Tested and Updated #30

--- exercises/30-computeAverageLengthOfWords/app.js
+++ exercises/30-computeAverageLengthOfWords/app.js
@@ -1,3 +1,1 @@
-function computeAverageLengthOfWords(word1, word2) {
-  // your code here
-}
+// Write your function here
--- exercises/30-computeAverageLengthOfWords/test.js
+++ exercises/30-computeAverageLengthOfWords/test.js
@@ -1,5 +1,10 @@
 const rewire = require ("rewire");
 
+test('Function computeAverageLengthOfWords must exist', () => {
+    const computeAverageLengthOfWords = rewire("./app.js").__get__("computeAverageLengthOfWords");
+    expect(computeAverageLengthOfWords).not.toBe(undefined);
+});
+
 test ('function should return the average of their lengths.', () => {
     const computeAverageLengthOfWords = rewire ('./app.js').__get__("computeAverageLengthOfWords");
 

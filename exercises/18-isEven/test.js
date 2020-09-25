d535b71c066e9a0c922b913164352246f65d303c
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 13:55:34 +0000

    Fixed #18

--- exercises/18-isEven/test.js
+++ exercises/18-isEven/test.js
@@ -1,5 +1,15 @@
 const rewire = require ("rewire");
 
+test('Function isEven must exist', () => {
+    const isEven = rewire("./app.js").__get__("isEven");
+    expect(isEven).not.toBe(undefined);
+});
+
+test('Function isEven must return true or false', () => {
+    const isEven = rewire("./app.js").__get__("isEven");
+    expect(isEven(20)).not.toBe(undefined);
+});
+
 test ('function should return whether the number given is even.', () => {
     const isEven = rewire ('./app.js').__get__("isEven");
     

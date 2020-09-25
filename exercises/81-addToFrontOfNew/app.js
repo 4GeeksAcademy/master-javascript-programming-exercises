243b2f1d0a749d7f7788ecc91b0844fe55b05bae
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 16:29:16 +0000

    Tested and Updated #81

--- exercises/81-addToFrontOfNew/app.js
+++ exercises/81-addToFrontOfNew/app.js
@@ -1,10 +1,1 @@
-function addToFrontOfNew(arr, element) {
-  // your code here
-  newArray = [];
-  newArray.unshift(element);
-  return newArray.concat(arr);
-}
-
-input = [1, 2];
-output = addToFrontOfNew(input, 3);
-console.log(output);
+// Write your function here
--- exercises/81-addToFrontOfNew/test.js
+++ exercises/81-addToFrontOfNew/test.js
@@ -1,5 +1,10 @@
 const rewire = require ("rewire");
 
+test('Function addToFrontOfNew must exist', () => {
+    const addToFrontOfNew = rewire("./app.js").__get__("addToFrontOfNew");
+    expect(addToFrontOfNew).not.toBe(undefined);
+});
+
 test ('The given object should be the same as the returned array by the function.', () => {
     const addToFrontOfNew = rewire ('./app.js').__get__("addToFrontOfNew");
      
@@ -16,7 +21,7 @@ test ('The returned array should have one more item than the original one .', ()
      expect(output.length>input.length).toBe(true);
 })
 
-test ('The returned array should have the number 3 as the first element of the array .', () => {
+test ('The returned array should have the given number as the first element of the array .', () => {
     const addToFrontOfNew = rewire ('./app.js').__get__("addToFrontOfNew");
      
     var input = [1, 2];

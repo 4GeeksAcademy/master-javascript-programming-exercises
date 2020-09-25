13e6123e43bf08f493d300b8919743a236ea04ba
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 18:44:31 +0000

    Tested and Updated #95

--- exercises/95-getNthElementOfProperty/app.js
+++ exercises/95-getNthElementOfProperty/app.js
@@ -1,12 +1,1 @@
-function getNthElementOfProperty(obj, key, n) {
-  // your code here
-    if(Array.isArray(obj[key])) {
-    return obj[key][n];
-}
-}
-
-var obj = {
-  key: [1, 2, 6]
-};
-var output = getNthElementOfProperty(obj, 'key', 1);
-console.log(output);
+// Write your function here

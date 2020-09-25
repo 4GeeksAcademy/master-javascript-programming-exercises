7df3978f02b4ccab5b0a5be1779f747f7b1040bc
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 18:19:42 +0000

    Tested and Updated #90

--- exercises/90-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/90-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,18 +1,1 @@
-function getElementsLessThan100AtProperty(obj, key) {
-  // your code here
-  let newArray = [];
-  for (key in obj){
-    for (i=0; i<obj[key].length; i++){
-      if (obj[key][i] < 100){
-         newArray.push(obj[key][i]);
-      }
-    }
-  }
-  return newArray;
-}
-
-var obj = {
-  key: [1000, 20, 50, 500]
-};
-var output = getElementsLessThan100AtProperty(obj, 'key');
-console.log(output);
+// Write your function here

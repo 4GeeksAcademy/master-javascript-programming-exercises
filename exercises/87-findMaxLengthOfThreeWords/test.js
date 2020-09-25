f320327b188d05bb38fddd0227ba7762c84defbd
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-09-25 18:11:38 +0000

    Tested and Updated #87

--- exercises/87-findMaxLengthOfThreeWords/test.js
+++ exercises/87-findMaxLengthOfThreeWords/test.js
@@ -1,5 +1,10 @@
 const rewire = require ("rewire");
 
+test('Function findMaxLengthOfThreeWords must exist', () => {
+    const findMaxLengthOfThreeWords = rewire("./app.js").__get__("findMaxLengthOfThreeWords");
+    expect(findMaxLengthOfThreeWords).not.toBe(undefined);
+});
+
 test ('The function should return the element with more characters.', () => {
     const findMaxLengthOfThreeWords = rewire ('./app.js').__get__("findMaxLengthOfThreeWords");
     var output = findMaxLengthOfThreeWords('past', 'present', 'future');

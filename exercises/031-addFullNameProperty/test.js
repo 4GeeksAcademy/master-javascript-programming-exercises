01dda181f7da9f95f966a0498b32ac1695161c12
Alejandro Sanchez <aalejo@gmail.com>
2021-03-03 16:25:09 +0000

    fixed two tests

--- exercises/031-addFullNameProperty/test.js
+++ exercises/031-addFullNameProperty/test.js
@@ -6,17 +6,30 @@ test('Function addFullNameProperty must exist', () => {
 });
 
 
-test('The function addFullNameProperty must return new object called "fullName"', () => {
-    const person = rewire("./app.js").__get__("person");
-    expect(person).toEqual({firstName: "Jade", lastName: "Smith", fullName: "Jade Smith"});
+test('The function addFullNameProperty must return an object', () => {
+    const addFullNameProperty = rewire("./app.js").__get__("addFullNameProperty");
+    let result = addFullNameProperty({});
+    expect(typeof(result)).toBe("object");
 });
 
-test('Must have person object', () => {
+test('The object that the function addFullNameProperty returns must include a fullName property', () => {
     const addFullNameProperty = rewire("./app.js").__get__("addFullNameProperty");
-    let person = {
-    firstName: 'Jade',
-    lastName: 'Smith'
-};
-    let output = addFullNameProperty(person)
-    expect(output).toBe('Jade Smith');
+    let result = addFullNameProperty({});
+    expect(typeof(result.fullName)).not.toBe("undefined");
+});
+
+
+test('Remember to include a space between firstName and lastName when creating the fullName property', () => {
+    const addFullNameProperty = rewire("./app.js").__get__("addFullNameProperty");
+    let result = addFullNameProperty({firstName: "Jade", lastName: "Smith"});
+
+    expect(result.fullName.includes(" ")).toBe(true);
+});
+
+
+test('The function addFullNameProperty must return new object called "fullName"', () => {
+    const addFullNameProperty = rewire("./app.js").__get__("addFullNameProperty");
+    let result = addFullNameProperty({firstName: "Jade", lastName: "Smith"});
+
+    expect(result).toEqual({firstName: "Jade", lastName: "Smith", fullName: "Jade Smith"});
 });
--- exercises/040-getLastElement/test.js
+++ exercises/040-getLastElement/test.js
@@ -7,16 +7,8 @@ test('Function getLastElement must exist', () => {
 
 test('Function must return the last value of the array', () => {
     const getLastElement = rewire("./app.js").__get__("getLastElement");
-    const output = getLastElement([95, 3], 1)
+    let output = getLastElement([95, 3])
     expect(output).toStrictEqual(3)
-})
-test('Function must return the last value of the array', () => {
-    const getLastElement = rewire("./app.js").__get__("getLastElement");
-    const output = getLastElement([1, 3, 9], 2)
+    output = getLastElement([1, 3, 9])
     expect(output).toStrictEqual(9)
-})
-test('Function must not return the 3rd value of the array', () => {
-    const getLastElement = rewire("./app.js").__get__("getLastElement");
-    const output = getLastElement([1, 3, 9, 10], 2)
-    expect(output).not.toBe(9)
 })

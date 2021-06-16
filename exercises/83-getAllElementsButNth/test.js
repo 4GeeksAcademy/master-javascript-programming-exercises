fcef7d0bc209034ff7d5b15f685c265d3556449b
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-06-16 15:51:12 +0000

    Issue #210 solved

--- exercises/83-getAllElementsButNth/test.js
+++ exercises/83-getAllElementsButNth/test.js
@@ -9,6 +9,6 @@ test ('The array returned should .', () => {
     const getAllElementsButNth = rewire ('./app.js').__get__("getAllElementsButNth");
      
      var output = getAllElementsButNth(['a', 'b', 'c'], 1);
-     expect(output).toContain(output[0],[2]);
+     expect(output).toContain('a', 'c');
 })
 

fa46141213d0c996a2d18d32545e09ce39d0276e
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-06-17 00:35:39 +0000

    Issue #186 fixed

--- exercises/91-countAllCharacters/app.js
+++ exercises/91-countAllCharacters/app.js
@@ -1,1 +1,13 @@
 // Write your function here
+function countAllCharacters(str) {
+    var obj = {};
+    for (var i=0; i<str.length; i++) {
+        if (obj[str[i]] == undefined) {
+            obj[str[i]] = 1;
+        } else { obj[str[i]] ++}
+    }
+    return obj;
+}
+
+var output = countAllCharacters('banana');
+console.log(output); // --> {b: 1, a: 3, n: 2}

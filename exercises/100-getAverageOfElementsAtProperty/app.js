89f2bf3ebcda9f35b73b031fea86988eb1b40642
Elvira Quiroz <elvira.quiroz.dp@gmail.com>
2021-10-13 14:30:51 +0000

    deleting solution ex 100

--- exercises/100-getAverageOfElementsAtProperty/app.js
+++ exercises/100-getAverageOfElementsAtProperty/app.js
@@ -1,21 +1,5 @@
 // function getAverageOfElementsAtProperty(obj, key) {
   // your code here
 // }
-var obj = {
-key: [1, 2, 3]
-};
 
-function getAverageOfElementsAtProperty(obj, key) {
-var count = 0;
-var average = 0;
-    if(obj[key]<1 || !Array.isArray(obj[key])){return 0;}
-    for(var prop in obj[key]){
-    count = count + obj[key][prop];
-}
 
-average = count/obj[key].length
-return average;
-}
-
-let output =getAverageOfElementsAtProperty(obj, 'key')
-console.log(output)

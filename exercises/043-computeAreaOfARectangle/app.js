c16e56288a34fe78ad7e775300cb511f86a83525
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-11-01 20:02:55 +0000

    Issue #361 fixed

--- exercises/043-computeAreaOfARectangle/app.js
+++ exercises/043-computeAreaOfARectangle/app.js
@@ -1,9 +1,7 @@
 function computeAreaOfARectangle(length, width) {
   // your code here
-  return length * width
-
 
 }
 
-var output = computeAreaOfARectangle(10, 18);
-console.log(40);
+let output = computeAreaOfARectangle(10, 18);
+console.log(output)

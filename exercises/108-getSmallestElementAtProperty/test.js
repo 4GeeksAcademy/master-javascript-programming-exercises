85f96200968f56b99d7347f23ae284577b499116
Martín Suárez <ms.prgramador@gmail.com>
2021-12-16 15:16:19 +0000

    exercise 108 with new test

--- exercises/108-getSmallestElementAtProperty/test.js
+++ exercises/108-getSmallestElementAtProperty/test.js
@@ -27,6 +27,12 @@ test('If the property in the given key is not an array, it should return an empt
     };
     var output = getSmallestElementAtProperty(obj, 'key')
     expect(output).toEqual([])
+
+    obj = {
+        key: {}
+    }
+    output = getSmallestElementAtProperty(obj, 'key')
+    expect(output).toBe([])
 })
 
 test('If there is no property on the key, it should return an empty array.', () => {

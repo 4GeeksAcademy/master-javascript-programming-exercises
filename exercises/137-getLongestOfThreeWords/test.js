18306797263b7a69a9abd05e5dbfd38b69abf7cf
Martín Suárez <ms.prgramador@gmail.com>
2022-01-17 16:10:06 +0000

    modified the test

--- exercises/137-getLongestOfThreeWords/test.js
+++ exercises/137-getLongestOfThreeWords/test.js
@@ -8,12 +8,11 @@ test('Function getLongestOfThreeWords must exist', () => {
 test('Given 3 words, return the longest of the three words.', () => {
     const getLongestOfThreeWords = rewire("./app.js").__get__("getLongestOfThreeWords");
     
-    expect(getLongestOfThreeWords('these', 'three', 'words')).toBe('these')
     expect(getLongestOfThreeWords('small', 'medium', 'large')).toBe('medium')
 })
 
 test('If they are the same length, it should return the first word found.', () => {
     const getLongestOfThreeWords = rewire("./app.js").__get__("getLongestOfThreeWords");
     
-    expect(getLongestOfThreeWords('three', 'tigers', 'three', 'tigers')).toBe('tigers')
+    expect(getLongestOfThreeWords('these', 'three', 'words')).toBe('these')
 })

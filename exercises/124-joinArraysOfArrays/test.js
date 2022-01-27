1c81d70641b932a000c4b4f287e77a04e1ed7a6a
Martín Suárez <ms.prgramador@gmail.com>
2022-01-27 05:39:41 +0000

    124-joinArraysOfArrays

--- exercises/124-joinArraysOfArrays/test.js
+++ exercises/124-joinArraysOfArrays/test.js
@@ -1,16 +1,24 @@
 const rewire = require("rewire");
+const joinArrayOfArrays = rewire('./app.js').__get__("joinArrayOfArrays");
+const path = require('path');
+const fs = require('fs');
+const app_content = fs.readFileSync(path.resolve(__dirname, './app.js'), 'utf8');
+
+
 
 test('Function joinArrayOfArrays must exist', () => {
-    const joinArrayOfArrays = rewire("./app.js").__get__("joinArrayOfArrays");
     expect(joinArrayOfArrays).not.toBe(undefined);
 });
 
+test('You need to use concat in your function', () => {
+    expect(app_content).toMatch(/\.concat/gm);
+});
+
 test('Function must returns a single array containing the elements of the nested arrays.', () => {
-    const joinArrayOfArrays = rewire('./app.js').__get__("joinArrayOfArrays");
-
-    let output = joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']])
-    expect(output).toEqual([1, 4, true, false, 'x', 'y']);
-
-    output = joinArrayOfArrays([[2, 6], [4, true]])
-    expect(output).toEqual([2, 6, 4, true]);
+    expect(joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']])).toEqual([1, 4, true, false, 'x', 'y']);
 })
+
+test("Your solution works with other entries", () => {
+    expect(joinArrayOfArrays([[2, 6], [4, true]])).toEqual([2, 6, 4, true]);
+})
+

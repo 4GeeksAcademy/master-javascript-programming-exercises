b3b5e4f7fefe065be3286c829513299c2dcbc021
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-02-01 16:13:14 -0400

    Update test.js

--- exercises/155-averageIntegers/test.js
+++ exercises/155-averageIntegers/test.js
@@ -5,6 +5,7 @@ const average = rewire("./app.js").__get__("average");
 test('Function average must exist', () => {
     expect(average).not.toBe(undefined);
 });
+
 test('Function sum must exist', () => {
     expect(sum).not.toBe(undefined);
 });
@@ -13,15 +14,14 @@ test('The function must sum the given numbers [4. 5] and return 9', () => {
     expect(sum([4, 5])).toBe(9)
 });
 
-test('The function must sum all the given numbers [4, 1, 12, 20, 1] and return ', () => {
+test('The function must sum all the given numbers [4, 1, 12, 20, 1] and return 38', () => {
     expect(sum([4, 1, 12, 20, 1])).toBe(38)
 });
 
 test('Function average must return the correct result of [5, 8]', () => {
     expect(average([5, 8])).toBe(6.5)
-    
 });
+
 test('Function average must return the correct result of [5, 15, 60, 2]', () => {
     expect(average([5, 15, 60, 2])).toBe(20.5)
-    
 });

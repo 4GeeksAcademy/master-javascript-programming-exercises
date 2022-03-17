168cb2566cfb8622e8296f80c8cf59b676925fed
Martín Suárez <ms.prgramador@gmail.com>
2022-03-17 17:21:43 +0000

    003-isOldEnoughToVote

--- exercises/003-isOldEnoughToVote/test.js
+++ exercises/003-isOldEnoughToVote/test.js
@@ -1,5 +1,16 @@
 const rewire = require ("rewire");
 
+
+test('The function isOldEnoughToVote must exist.', () => {
+    const result = rewire ('./app.js').__get__("isOldEnoughToVote");
+    expect(result).toBeTruthy();
+})
+
+test('The function isOldEnoughToVote must return something.', () => {
+    const result = rewire ('./app.js').__get__("isOldEnoughToVote");
+    expect(result()).not.toBe(undefined);
+})
+
 test ('is old enough to legally vote in the United States.', () => {
     const isOldEnoughToVote = rewire ('./app.js').__get__("isOldEnoughToVote");
 

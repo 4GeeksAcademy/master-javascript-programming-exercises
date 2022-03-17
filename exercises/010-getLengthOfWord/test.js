577f0fe1bce1f3165166a9b671ef7cc55678e3c2
Martín Suárez <ms.prgramador@gmail.com>
2022-03-17 17:57:06 +0000

    010-getLengthOfWord

--- exercises/010-getLengthOfWord/test.js
+++ exercises/010-getLengthOfWord/test.js
@@ -1,8 +1,23 @@
 const rewire = require ("rewire");
 
+test('The funciton getLengthOfWord must exist.',()=>{
+    const getLengthOfWord = rewire('./app.js').__get__('getLengthOfWord');
+    expect(getLengthOfWord).toBeTruthy();
+})
+test('The funciton getLengthOfWord must return something.',()=>{
+    const getLengthOfWord = rewire('./app.js').__get__('getLengthOfWord');
+    expect(getLengthOfWord()).not.toBe(undefined);
+})
+
 test ('function should return the length of the given word.', () => {
     const getLengthOfWord = rewire ('./app.js').__get__("getLengthOfWord");
 
     expect(getLengthOfWord('some')).toBe(4);
+    
+})
+test ('function should return the length of the given word. Testing with somemorewordshere', () => {
+    const getLengthOfWord = rewire ('./app.js').__get__("getLengthOfWord");
+
     expect(getLengthOfWord('somemorewordshere')).toBe(17);
+    
 })

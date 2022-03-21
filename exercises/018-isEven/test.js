e33b62c08e3420153f07c4a118407fbcd044505a
Martín Suárez <ms.prgramador@gmail.com>
2022-03-21 18:42:51 +0000

    018-isEven

--- exercises/018-isEven/test.js
+++ exercises/018-isEven/test.js
@@ -1,23 +1,26 @@
 const rewire = require ("rewire");
-
+const isEven = rewire ('./app.js').__get__("isEven");
 test('Function isEven must exist', () => {
-    const isEven = rewire("./app.js").__get__("isEven");
     expect(isEven).not.toBe(undefined);
 });
 
 test('Function isEven must return soomething', () => {
-    const isEven = rewire("./app.js").__get__("isEven");
     expect(isEven(20)).not.toBe(undefined);
 });
 
 test('Function isEven must return a boolean (true or false)', () => {
-    const isEven = rewire("./app.js").__get__("isEven");
     expect(typeof(isEven(20))).toBe("boolean");
 });
 
-test ('Tried the function with 11 and it did not return false.', () => {
-    const isEven = rewire ('./app.js').__get__("isEven");
-    
+test ('The function must return false when the number isn\'t even. Testing with 11', () => {
     expect(isEven(11)).toBe(false);
-    expect(isEven(10)).toBe(true);
+})
+test ('The function must return false when the number isn\'t even. Testing with 63', () => {
+    expect(isEven(63)).toBe(false);
+})
+test ('The function must return true when the number is even. Testing with 12', () => {
+    expect(isEven(12)).toBe(true);
+})
+test ('The function must return true when the number is even. Testing with 4', () => {
+    expect(isEven(4)).toBe(true);
 })

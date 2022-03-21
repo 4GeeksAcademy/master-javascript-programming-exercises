359efa5e4c34d082d51d8b4528d010ebebd26f9e
Martín Suárez <ms.prgramador@gmail.com>
2022-03-21 17:08:50 +0000

    012-isGreaterThanTen

--- exercises/012-isGreaterThanTen/test.js
+++ exercises/012-isGreaterThanTen/test.js
@@ -1,8 +1,18 @@
 const rewire = require ("rewire");
+const isGreaterThanTen = rewire('./app.js').__get__('isGreaterThanTen');
 
-test ('function should return whether it is greater than 10.', () => {
-    const isGreaterThanTen = rewire ('./app.js').__get__("isGreaterThanTen");
+test('The function isGreaterThanTen must exist.',()=>{
+    expect(isGreaterThanTen).toBeTruthy();
+})
 
-    expect(isGreaterThanTen(11)).toBe(true);
+test('The function isGreaterThanTen must return something.',()=>{
+    expect(isGreaterThanTen()).not.toBe(undefined)
+})
+
+test ('function should return the expected output when the number is less or equal than 10.', () => {
     expect(isGreaterThanTen(10)).toBe(false);
+})
+
+test ('function should return the expected output when the number is grater than 10.', () => {
+    expect(isGreaterThanTen(11)).toBe(true);
 })

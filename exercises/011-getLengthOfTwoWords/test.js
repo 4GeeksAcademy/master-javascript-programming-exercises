8f714488cfe240547da6e5c308c9bc54473836b5
Martín Suárez <ms.prgramador@gmail.com>
2022-03-21 16:56:56 +0000

    011-getLengthOfTwoWords

--- exercises/011-getLengthOfTwoWords/test.js
+++ exercises/011-getLengthOfTwoWords/test.js
@@ -1,8 +1,17 @@
 const rewire = require ("rewire");
+const getLengthOfTwoWords = rewire('./app.js').__get__('getLengthOfTwoWords');
 
-test ('function should return the sum of their lengths.', () => {
-    const getLengthOfTwoWords = rewire ('./app.js').__get__("getLengthOfTwoWords");
+test('The function getLengthOfTwoWords must exist.', ()=>{
+    expect(getLengthOfTwoWords).toBeTruthy();
+})
 
+test('The function getLengthOfTwoWords should return something.',()=>{
+    expect(getLengthOfTwoWords()).not.toBe(undefined);
+})
+
+test ("function should return the sum of their lengths. Testing with 'some', 'words'", () => {
     expect(getLengthOfTwoWords('some', 'words')).toBe(9);
+})
+test ("function should return the sum of their lengths. Testing with 'juana', 'lacubana'", () => {
     expect(getLengthOfTwoWords('juana', 'lacubana')).toBe(13);
 })

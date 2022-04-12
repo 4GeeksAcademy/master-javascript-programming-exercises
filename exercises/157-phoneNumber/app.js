009a94ab06729b27c6e31bc6e0cd4a28918f4c41
Martín Suárez <ms.prgramador@gmail.com>
2022-04-12 19:07:04 +0000

    157-phoneNumber

--- exercises/157-phoneNumber/app.js
+++ exercises/157-phoneNumber/app.js
@@ -5,23 +5,19 @@ function PhoneNumberFormatter(numbers) {
 PhoneNumberFormatter.prototype.render = function() {
   var string = '';
   // your code here
-  string = this.parenthesize(this.getAreaCode()) + " " + this.getExchangeCode() + "-" + this.getLineNumber();
   return string;
 };
 
 PhoneNumberFormatter.prototype.getAreaCode = function() {
   // your code here
-  return this.slice(0,3);
 };
 
 PhoneNumberFormatter.prototype.getExchangeCode = function() {
   // your code here
-  return this.slice(3,6);
 };
 
 PhoneNumberFormatter.prototype.getLineNumber = function() {
   // your code here
-  return this.slice(6,10);
 };
 
 PhoneNumberFormatter.prototype.parenthesize = function(string) {
@@ -31,6 +27,3 @@ PhoneNumberFormatter.prototype.parenthesize = function(string) {
 PhoneNumberFormatter.prototype.slice = function(start, end) {
   return this.numbers.slice(start, end).join('');
 };
-
-let number = new PhoneNumberFormatter([6,5,0,8,3,5,9,1,7,2]);
-console.log(number.render());

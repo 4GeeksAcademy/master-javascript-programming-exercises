92ecf2e4439ea9a18ac1dc570dcb5027dab1811e
Martín Suárez <ms.prgramador@gmail.com>
2022-04-21 15:39:01 +0000

    022-isEitherEven

--- exercises/022-isEitherEven/test.js
+++ exercises/022-isEitherEven/test.js
@@ -1,19 +1,20 @@
 const rewire = require ("rewire");
-
+const isEitherEven = rewire ('./app.js').__get__("isEitherEven");
 test('Function isEitherEven must exist', () => {
-    const isEitherEven = rewire("./app.js").__get__("isEitherEven");
     expect(isEitherEven).not.toBe(undefined);
 });
 
 
 test('Function isEitherEven must return boolean', () => {
-    const isEitherEven = rewire("./app.js").__get__("isEitherEven");
     expect(typeof(isEitherEven(3,3)) == "boolean" || isEitherEven(3,3) === 0).toBeTruthy()
 });
 
-test ('Tested with (7,12) & (15,17) and it should have returned true and false', () => {
-    const isEitherEven = rewire ('./app.js').__get__("isEitherEven");
-
+test ('The function must return true when one of the given numbers is even. Tested with (7,12)', () => {
     expect(isEitherEven(7, 12)).toBe(true);
-    expect(isEitherEven(15, 17)).toBe(false);
+})
+test ('The function must return true when one of the given numbers is even. Tested with (122,3)', () => {
+    expect(isEitherEven(122, 3)).toBe(true);
+})
+test ('The function must return false when none of the given numbers is even. Tested with (99,5)', () => {
+    expect(isEitherEven(99, 5)).toBe(false);
 })

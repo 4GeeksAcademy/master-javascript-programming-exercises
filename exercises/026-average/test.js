bfd5a7e4b03b9f536fdf3e439593f0debd7adfdc
Martín Suárez <ms.prgramador@gmail.com>
2022-06-20 04:28:27 +0000

    ex 026 to 030

--- exercises/026-average/test.js
+++ exercises/026-average/test.js
@@ -1,13 +1,16 @@
 const rewire = require ("rewire");
+const average = rewire("./app.js").__get__("average");
 
 test('Function average must exist', () => {
-    const average = rewire("./app.js").__get__("average");
     expect(average).not.toBe(undefined);
 });
+test('Function should return a number', () => {
+    expect(typeof(average(1,2))).toBe("number");
+});
 
-test ('function should returns their average.', () => {
-    const average = rewire ('./app.js').__get__("average");
-
+test ('The function should returns the correct average to the given params (24, 36).', () => {
     expect(average(24, 36)).toBe(30);
+})
+test ('The function should returns the correct average to the given params (102, 93).', () => {
     expect(average(102, 93)).toBe(97.5);
 })
--- exercises/028-cube/test.js
+++ exercises/028-cube/test.js
@@ -1,13 +1,16 @@
 const rewire = require ("rewire");
+const cube = rewire("./app.js").__get__("cube");
 
 test('Function cube must exist', () => {
-    const cube = rewire("./app.js").__get__("cube");
     expect(cube).not.toBe(undefined);
 });
+test('Function cube should return a number', () => {
+    expect(typeof cube(1)).toBe('number');
+});
 
-test ('function should return the cube of the number given.', () => {
-    const cube = rewire ('./app.js').__get__("cube");
-
-    expect(cube(24)).toBe(13824);
+test ('The function should return the cube of the number given. Testing with (93)', () => {
     expect(cube(93)).toBe(804357);
+})
+test ('function should return the cube of the number given. Testing with (24)', () => {
+    expect(cube(24)).toBe(13824);
 })

e9ca52c10d90e9ca547b779f7b026ece6fe6f4c0
Martín Suárez <ms.prgramador@gmail.com>
2022-06-30 01:04:21 +0000

    035 to 040

--- exercises/036-isPersonOldEnoughToDrink/test.js
+++ exercises/036-isPersonOldEnoughToDrink/test.js
@@ -1,16 +1,28 @@
 const rewire = require ("rewire");
+const isPersonOldEnoughToDrink = rewire("./app.js").__get__("isPersonOldEnoughToDrink");
 
 test('Function isPersonOldEnoughToDrink must exist', () => {
-    const isPersonOldEnoughToDrink = rewire("./app.js").__get__("isPersonOldEnoughToDrink");
+
     expect(isPersonOldEnoughToDrink).not.toBe(undefined);
+});
+test('Function isPersonOldEnoughToDrink must return something', () => {
+
+    expect(isPersonOldEnoughToDrink({})).not.toBe(undefined);
 });
 
 test ('Function should return a false if age less than 21', () => {
-    const isPersonOldEnoughToDrink = rewire ('./app.js').__get__("isPersonOldEnoughToDrink");
+   
     expect(isPersonOldEnoughToDrink({ age: 20 })).toBe(false);
 })
+test ('Function should return a false if age less than 21', () => {
+   
+    expect(isPersonOldEnoughToDrink({ age: 7 })).toBe(false);
+})
 test ('Function should return a true if age are greather than or equal to 21', () => {
-    const isPersonOldEnoughToDrink = rewire ('./app.js').__get__("isPersonOldEnoughToDrink");
+    
     expect(isPersonOldEnoughToDrink({ age: 21 })).toBe(true);
+})
+test ('Function should return a true if age are greather than or equal to 21', () => {
+    
     expect(isPersonOldEnoughToDrink({ age: 24 })).toBe(true);
 })

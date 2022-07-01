69e83c42819c33b56acf5a5e64b425b66c5a0911
Martín Suárez <ms.prgramador@gmail.com>
2022-07-01 15:21:53 +0000

    056-060

--- exercises/056-getAllElementsButFirst/test.js
+++ exercises/056-getAllElementsButFirst/test.js
@@ -1,16 +1,26 @@
 const rewire = require("rewire");
+const getAllElementsButFirst = rewire("./app.js").__get__("getAllElementsButFirst");
 
 test('Function getAllElementsButFirst must exist', () => {
-    const getAllElementsButFirst = rewire("./app.js").__get__("getAllElementsButFirst");
+    
     expect(getAllElementsButFirst).not.toBe(undefined);
 });
+test('Function getAllElementsButFirst must return something', () => {
+    
+    expect(getAllElementsButFirst([1,2])).not.toBe(undefined);
+});
+test('Function getAllElementsButFirst must return a list', () => {
+    
+    expect(typeof getAllElementsButFirst([1,2])).toBe("object");
+});
 
-test('Function must return an array with all elements but not the first', () => {
-    const getAllElementsButFirst = rewire('./app.js').__get__("getAllElementsButFirst");
-
-    var output = getAllElementsButFirst([1, 2, 3, 4])
-    expect(output).toEqual([2, 3, 4])
+test('Function must return an array with all elements but not the first. Testing with [3,6,9,12]', () => {
 
     output = getAllElementsButFirst([3, 6, 9, 12])
     expect(output).toEqual([6, 9, 12])
+})
+test('Function must return an array with all elements but not the first. Testing with [1,2,3,4]', () => {
+
+    var output = getAllElementsButFirst([1, 2, 3, 4])
+    expect(output).toEqual([2, 3, 4])
 })
--- exercises/060-removeFromFrontOfNew/test.js
+++ exercises/060-removeFromFrontOfNew/test.js
@@ -1,16 +1,26 @@
 const rewire = require("rewire");
+const removeFromFrontOfNew = rewire("./app.js").__get__("removeFromFrontOfNew");
 
 test('Function removeFromFrontOfNew must exist', () => {
-    const removeFromFrontOfNew = rewire("./app.js").__get__("removeFromFrontOfNew");
+    
     expect(removeFromFrontOfNew).not.toBe(undefined);
 });
+test('Function removeFromFrontOfNew must return something', () => {
+    
+    expect(removeFromFrontOfNew([1,2])).not.toBe(undefined);
+});
+test('Function removeFromFrontOfNew must return a list', () => {
+    
+    expect(typeof removeFromFrontOfNew([1,2])).toBe("object");
+});
 
-test('Function must returns a new array containing all elements, ignoring the first', () => {
-    const removeFromFrontOfNew = rewire('./app.js').__get__("removeFromFrontOfNew");
-
-    var output = removeFromFrontOfNew([1, 2, 3])
-    expect(output).toEqual([2, 3])
+test('Function must returns a new array containing all elements, ignoring the first. Testing with [9,21,74,33]', () => {
 
     output = removeFromFrontOfNew([9, 21, 74, 33])
     expect(output).toEqual([21, 74, 33])
+})
+test('Function must returns a new array containing all elements, ignoring the first. Testing with [1,2,3]', () => {
+
+    var output = removeFromFrontOfNew([1, 2, 3])
+    expect(output).toEqual([2, 3])
 })

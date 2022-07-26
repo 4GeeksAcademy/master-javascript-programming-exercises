0c622ea5302a81e7cfc6e02ad88c1cf204c3302c
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-07-26 03:52:31 +0000

    Tests improved

--- exercises/042-addToBack/test.js
+++ exercises/042-addToBack/test.js
@@ -9,17 +9,17 @@ test('Function addToBack must return something', () => {
     
     expect(addToBack([],2)).not.toBe(undefined);
 });
-test('Function addToBack must return a number', () => {
+test('Function addToBack must return an array', () => {
     
     expect(typeof addToBack([],2)).toBe("object");
 });
 
-test('The function must return the given number in the last position of the array', () => {
+test('The function must return the given number in the last position of the array.', () => {
 
     expect(addToBack([1, 2], 3)).toStrictEqual([1, 2, 3]);
 });
 
-test('The function must return the given number in the last position of the array', () => {
+test('The function must return the given number in the last position of the array. Testing with different values', () => {
 
     expect(addToBack([22, 44], 11)).toStrictEqual([22, 44, 11]);
 });
--- exercises/046-computeTripledAreaOfARectangle/test.js
+++ exercises/046-computeTripledAreaOfARectangle/test.js
@@ -2,23 +2,18 @@ const rewire = require ("rewire");
 const computeTripledAreaOfARectangle = rewire("./app.js").__get__("computeTripledAreaOfARectangle");
 
 test('Function computeTripledAreaOfARectangle must exist', () => {
-    
     expect(computeTripledAreaOfARectangle).not.toBe(undefined);
 });
 test('Function computeTripledAreaOfARectangle must return something', () => {
-    
     expect(computeTripledAreaOfARectangle(1,1)).not.toBe(undefined);
 });
 test('Function computeTripledAreaOfARectangle must return a number', () => {
-    
     expect(typeof computeTripledAreaOfARectangle(1,1)).toBe("number");
 });
 
-test('The fuction should return the area of the rectangle multiplied by 3. Testing with 2,4', () => {
-    
+test('The fuction should return the area of the rectangle multiplied by 3. Testing with 2 and 4.', () => {
     expect(computeTripledAreaOfARectangle(2, 4)).toBe(24)
 })
-test('The fuction should return the area of the rectangle multiplied by 3. Testing with 5,3', () => {
-    
+test('The fuction should return the area of the rectangle multiplied by 3. Testing with 3 and 5.', () => {
     expect(computeTripledAreaOfARectangle(5, 3)).toBe(45)
 })

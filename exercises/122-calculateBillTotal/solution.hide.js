08facbd594b826b8836f49fa794d30fee5025432
kiddopro <ms.prgramador@gmail.com>
2022-07-27 01:30:12 -0300

    ex 121 to 123

---
+++ exercises/122-calculateBillTotal/solution.hide.js
@@ -0,0 +1,8 @@
+function calculateBillTotal(preTaxAndTipAmount) {
+  // your code here
+  let tax = preTaxAndTipAmount * 0.095;
+  let tip = preTaxAndTipAmount * 0.15;
+  return preTaxAndTipAmount + tax + tip;
+}
+var output = calculateBillTotal(20);
+console.log(output); // --> 24.9

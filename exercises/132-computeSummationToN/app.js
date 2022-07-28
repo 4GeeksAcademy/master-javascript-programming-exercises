b574936dda1254a9c74eb5e58b0fcb11683374ce
kiddopro <ms.prgramador@gmail.com>
2022-07-28 11:59:03 -0300

    ex 131 to 132

--- exercises/132-computeSummationToN/app.js
+++ exercises/132-computeSummationToN/app.js
@@ -1,5 +1,8 @@
 function computeSummationToN(n) {
-    // your code here
+  // your code here
+  let aux = 0;
+  for (let e = 1; e < n + 1; e++) aux += e;
+  return aux;
 }
 
 var output = computeSummationToN(6);

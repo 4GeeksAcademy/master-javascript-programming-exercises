9b8a2a2b163608b40dfb48c9844e42e7649799f9
kiddopro <ms.prgramador@gmail.com>
2022-07-29 00:24:34 -0300

    ex 133

--- exercises/133-convertScoreToGrade/test.js
+++ exercises/133-convertScoreToGrade/test.js
@@ -1,42 +1,36 @@
-const rewire = require("rewire");
+const rewire = require('rewire');
+const convertScoreToGrade = rewire('./app.js').__get__('convertScoreToGrade');
 
 test('Function convertScoreToGrade must exist', () => {
-    const convertScoreToGrade = rewire("./app.js").__get__("convertScoreToGrade");
-    expect(convertScoreToGrade).not.toBe(undefined);
+  expect(convertScoreToGrade).not.toBe(undefined);
+});
+test('Function convertScoreToGrade must return something', () => {
+  expect(convertScoreToGrade(1)).not.toBe(undefined);
+});
+test('Function convertScoreToGrade must return a string', () => {
+  expect(typeof convertScoreToGrade(1)).toBe('string');
 });
 
 test('if the punctuation is from 100 to 90 it must return grade "A".', () => {
-    const convertScoreToGrade = rewire('./app.js').__get__("convertScoreToGrade");
-
-    expect(convertScoreToGrade(91)).toBe('A');
-})
+  expect(convertScoreToGrade(91)).toBe('A');
+});
 
 test('if the punctuation is from 89 to 80 it must return grade "B".', () => {
-    const convertScoreToGrade = rewire('./app.js').__get__("convertScoreToGrade");
-
-    expect(convertScoreToGrade(83)).toBe('B');
-})
+  expect(convertScoreToGrade(83)).toBe('B');
+});
 
 test('if the punctuation is from 79 to 70 it must return grade "C".', () => {
-    const convertScoreToGrade = rewire('./app.js').__get__("convertScoreToGrade");
-
-    expect(convertScoreToGrade(78)).toBe('C');
-})
+  expect(convertScoreToGrade(78)).toBe('C');
+});
 
 test('if the punctuation is from 69 to 60 it must return grade "D".', () => {
-    const convertScoreToGrade = rewire('./app.js').__get__("convertScoreToGrade");
-
-    expect(convertScoreToGrade(68)).toBe('D');
-})
+  expect(convertScoreToGrade(68)).toBe('D');
+});
 
 test('if the punctuation is from 59 to 0 it must return grade "F".', () => {
-    const convertScoreToGrade = rewire('./app.js').__get__("convertScoreToGrade");
-
-    expect(convertScoreToGrade(42)).toBe('F');
-})
+  expect(convertScoreToGrade(42)).toBe('F');
+});
 
 test('If the given score is greater than 100 or less than 0, it should return "INVALID SCORE".', () => {
-    const convertScoreToGrade = rewire('./app.js').__get__("convertScoreToGrade");
-
-    expect(convertScoreToGrade(104)).toBe('INVALID SCORE');
-})
+  expect(convertScoreToGrade(104)).toBe('INVALID SCORE');
+});

39fe3d2f2a4fa61f710e47df0180d0abb7f2f71a
kiddopro <ms.prgramador@gmail.com>
2022-07-30 13:05:56 -0300

    139 to 140

---
+++ exercises/140-modulo/solution.hide.js
@@ -0,0 +1,7 @@
+function modulo(num1, num2) {
+  // your code here
+  return num1 % num2;
+}
+
+var output = modulo(25, 4);
+console.log(output); // --> 1
--- exercises/140-modulo/test.js
+++ exercises/140-modulo/test.js
@@ -1,26 +1,30 @@
-const rewire = require("rewire");
+const rewire = require('rewire');
+const modulo = rewire('./app.js').__get__('modulo');
 
 test('Function modulo must exist', () => {
-    const modulo = rewire("./app.js").__get__("modulo");
-    expect(modulo).not.toBe(undefined);
+  expect(modulo).not.toBe(undefined);
+});
+test('Function modulo must return something', () => {
+  expect(modulo(1, 2)).not.toBe(undefined);
+});
+test('Function modulo must return a number', () => {
+  expect(typeof modulo(1, 2)).toBe('number');
 });
 
-test('Given 2 numbers, return the remainder after dividing num1 by num2.', () => {
-    const modulo = rewire("./app.js").__get__("modulo");
+test('Given 2 numbers, return the remainder after dividing num1 by num2. Testing with (5, 55).', () => {
+  expect(modulo(5, 55)).toBe(5);
+});
+test('Given 2 numbers, return the remainder after dividing num1 by num2.Testing with (25, 4).', () => {
+  expect(modulo(25, 4)).toBe(1);
+});
 
-    expect(modulo(25, 4)).toBe(1)
-    expect(modulo(5, 55)).toBe(5)
-})
+test('The module always returns the sign of the first number. Testing with (-5, 55).', () => {
+  expect(modulo(-5, 55)).toBe(-5);
+});
+test('The module always returns the sign of the first number. Testing with (-25, 4).', () => {
+  expect(modulo(-25, 4)).toBe(-1);
+});
 
-test('The module always returns the sign of the first number.', () => {
-     const modulo = rewire("./app.js").__get__("modulo");
-
-    expect(modulo(-25, 4)).toBe(-1)
-    expect(modulo(-5, 55)).toBe(-5)
-})
-
-test('If any of the operators is NaN, then the result is NaN.', () => {
-    const modulo = rewire("./app.js").__get__("modulo");
-
-    expect(modulo(25, 'hey')).toBe(NaN)
-})
+test('If any of the operators is different from a number, then the result is NaN.', () => {
+  expect(modulo(25, 'hey')).toBe(NaN);
+});

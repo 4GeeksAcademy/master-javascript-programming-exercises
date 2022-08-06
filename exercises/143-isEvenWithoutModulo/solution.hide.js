f860172c625af26d579b55a282de30b0e1947368
kiddopro <ms.prgramador@gmail.com>
2022-08-06 00:36:50 -0300

    ex 143 and 144

---
+++ exercises/143-isEvenWithoutModulo/solution.hide.js
@@ -0,0 +1,7 @@
+function isEvenWithoutModulo(num) {
+  // your code here
+  if (num % 2 == 0) return true;
+  else return false;
+}
+let output = isEvenWithoutModulo(8);
+console.log(output); // --> true
--- exercises/144-multiplyBetween/test.js
+++ exercises/144-multiplyBetween/test.js
@@ -1,19 +1,23 @@
-const rewire = require("rewire");
+const rewire = require('rewire');
+const multiplyBetween = rewire('./app.js').__get__('multiplyBetween');
 
 test('Function multiplyBetween must exist', () => {
-    const multiplyBetween = rewire("./app.js").__get__("multiplyBetween");
-    expect(multiplyBetween).not.toBe(undefined);
+  expect(multiplyBetween).not.toBe(undefined);
+});
+test('Function multiplyBetween must return something', () => {
+  expect(multiplyBetween(1, 2)).not.toBe(undefined);
+});
+test('Function multiplyBetween must return a number', () => {
+  expect(typeof multiplyBetween(1, 2)).toBe('number');
 });
 
-test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2.', () => {
-    const multiplyBetween = rewire("./app.js").__get__("multiplyBetween");
-
-    expect(multiplyBetween(2, 5)).toBe(24)
-    expect(multiplyBetween(4, 8)).toBe(840)
-})
+test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2. Testing with 2, 5.', () => {
+  expect(multiplyBetween(2, 5)).toBe(24);
+});
+test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2. Testing with 4, 8.', () => {
+  expect(multiplyBetween(4, 8)).toBe(840);
+});
 
 test('If num2 is not greater than num1, it should return 0.', () => {
-    const multiplyBetween = rewire("./app.js").__get__("multiplyBetween");
-
-    expect(multiplyBetween(3, 2)).toBe(0)
-})
+  expect(multiplyBetween(3, 2)).toBe(0);
+});

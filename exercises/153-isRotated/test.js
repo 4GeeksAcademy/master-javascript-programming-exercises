7327a5797407c7e318b1e4f0ce6886518bd352fd
kiddopro <ms.prgramador@gmail.com>
2022-08-07 19:04:59 -0300

    ex 153

--- exercises/153-isRotated/test.js
+++ exercises/153-isRotated/test.js
@@ -1,14 +1,25 @@
-const rewire = require("rewire");
+const rewire = require('rewire');
+const isRotated = rewire('./app.js').__get__('isRotated');
 
 test('Function isRotated must exist', () => {
-    const isRotated = rewire("./app.js").__get__("isRotated");
-    expect(isRotated).not.toBe(undefined);
+  expect(isRotated).not.toBe(undefined);
+});
+test('Function isRotated must return something', () => {
+  expect(isRotated('test', 'tste')).not.toBe(undefined);
+});
+test('Function isRotated must return a boolean', () => {
+  expect(typeof isRotated('test', 'tste')).toBe('boolean');
 });
 
 test('The function should return true or false if there is one of the 2 given strings rotated.', () => {
-    const isRotated = rewire("./app.js").__get__("isRotated");
-
-    expect(isRotated('hello world', 'orldhello w')).toBe(true)
-    expect(isRotated('hello world', 'asdmol owm')).toBe(false)
-    expect(isRotated('hola mundo', 'dohola mun')).toBe(true)
-})
+  expect(isRotated('hello world', 'orldhello w')).toBe(true);
+});
+test('The function should return true or false if there is one of the 2 given strings rotated.', () => {
+  expect(isRotated('Test ', 'stet')).toBe(false);
+});
+test('The function should return true or false if there is one of the 2 given strings rotated.', () => {
+  expect(isRotated('Learning is fun', 'Lr aen ngiiufs')).toBe(true);
+});
+test('The function should return true or false if there is one of the 2 given strings rotated.', () => {
+  expect(isRotated('Work from Home', 'kfmoemro Hor W')).toBe(true);
+});

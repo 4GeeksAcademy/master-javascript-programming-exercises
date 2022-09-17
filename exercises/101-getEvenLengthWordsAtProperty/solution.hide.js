1bc9a20ef79e51a31132b755fd5d24455cee3e37
Martín Suárez <ms.prgramador@gmail.com>
2022-09-17 23:17:42 +0000

    ex 101 with solution.hide.js

---
+++ exercises/101-getEvenLengthWordsAtProperty/solution.hide.js
@@ -0,0 +1,19 @@
+var obj = {
+    key: ['a', 'long', 'game']
+  };
+  function getEvenLengthWordsAtProperty(obj, key) {
+      // your code here
+  
+      if (!obj.key || !Array.isArray(obj.key)){
+        return []
+      }else{
+        let aux = []
+        obj.key.map(e=>{
+          e.length % 2 == 0 ? aux.push(e) : null
+        })
+        return aux;
+      }
+  }
+  
+  var output = getEvenLengthWordsAtProperty(obj, 'key');
+  console.log(output); // --> ['long', 'game']

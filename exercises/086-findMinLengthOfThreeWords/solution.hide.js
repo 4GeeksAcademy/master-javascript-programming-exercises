0880610c2e387ec65580971fc4d9e5e07014c0f3
ErnestoXG <ernestox.061@gmail.com>
2022-09-19 21:07:11 +0000

    Added solutions

---
+++ exercises/086-findMinLengthOfThreeWords/solution.hide.js
@@ -0,0 +1,9 @@
+function findMinLengthOfThreeWords(a, b, c){
+    if(a.length < b.length && a.length < c.length){
+        return a.length
+    }else if(b.length < a.length && b.length < c.length){
+        return b.length
+    }else{
+        return c.length
+    }
+}

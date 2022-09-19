c017eef836385464632fb84e433a7b4de14dccec
ErnestoXG <ernestox.061@gmail.com>
2022-09-19 19:33:50 +0000

    added solution

---
+++ exercises/082-addToBackOfNew/solution.hide.js
@@ -0,0 +1,15 @@
+function addToBackOfNew(arr, element) {
+    // Write your function here
+    let newArr = []
+    arr.forEach(e=>{
+        newArr.push(e)
+    })
+
+    newArr.push(element)
+    return newArr
+}
+
+let input = [1, 2];
+let output = addToBackOfNew(input, 3);
+console.log(input); // --> [1, 2]
+console.log(output); // --> [1, 2, 3]

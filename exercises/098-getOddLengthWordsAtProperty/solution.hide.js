a64a985b282753f86672035820693576d07fa286
ErnestoXG <ernestox.061@gmail.com>
2022-09-20 00:00:43 +0000

    Added solution

---
+++ exercises/098-getOddLengthWordsAtProperty/solution.hide.js
@@ -0,0 +1,14 @@
+// Write your function here
+function getOddLengthWordsAtProperty(obj, key){
+    if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length == 0){
+        return []
+    }
+
+    let newArr = [];
+    for(let i = 0; i < obj[key].length; i++){
+        if(obj[key][i].length % 3 == 0){
+            newArr.push(obj[key][i])
+        }
+    }
+    return newArr;
+}

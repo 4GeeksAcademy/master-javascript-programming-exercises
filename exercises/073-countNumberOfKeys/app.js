0dc3c69c4d7f77c1d9c859f5183c7457656d4815
dsilva06 <diegosilva06092001@gmail.com>
2022-11-17 20:12:19 +0000

    fixed app.js && tests.js

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,4 +1,14 @@
+let obj = {
+    person:"Rafael",
+    lastName:"Leao",
+    country: "Italy",
+    city: "Milan",
+    phoneNum:123-456-789,
+    jerseyNum:27,
+}
+
 function countNumberOfKeys(obj) {
     // your code here
-
+   return Object.keys(obj).length
 }
+console.log(countNumberOfKeys(obj))

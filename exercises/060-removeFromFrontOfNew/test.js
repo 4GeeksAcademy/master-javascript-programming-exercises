2c45c6d5f69cdad414ad522371cb1dfb9591404d
ErnestoXG <ernestox.061@gmail.com>
2023-02-07 19:37:24 +0000

    Clarified instructions, added solution, fixed test

--- exercises/060-removeFromFrontOfNew/test.js
+++ exercises/060-removeFromFrontOfNew/test.js
@@ -14,11 +14,15 @@ test('Function removeFromFrontOfNew must return a list', () => {
 });
 
 test('Function must returns a new array containing all elements, ignoring the first. Testing with [9,21,74,33]', () => {
-    let output = removeFromFrontOfNew([9, 21, 74, 33])
+    let input = [9, 21, 74, 33]
+    let output = removeFromFrontOfNew(input)
     expect(output).toEqual([21, 74, 33])
+    expect(input).toEqual([9, 21, 74, 33])
 });
 
 test('Function must returns a new array containing all elements, ignoring the first. Testing with [1,2,3]', () => {
-    let output = removeFromFrontOfNew([1, 2, 3])
+    let input = [1, 2, 3]
+    let output = removeFromFrontOfNew(input)
     expect(output).toEqual([2, 3])
+    expect(input).toEqual([1, 2, 3])
 });

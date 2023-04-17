60f3938413c877b71bd8c9391595d3ad9dc3a9a2
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 13:56:10 +0200

    Update app.js

--- exercises/012-isGreaterThanTen/app.js
+++ exercises/012-isGreaterThanTen/app.js
@@ -1,3 +1,11 @@
 function isGreaterThanTen(num) {
   // your code here
+  if (num > 10){
+    return true
+  } else {
+    return false
+  }
 }
+
+let output = isGreaterThanTen(11);
+console.log(output); // --> true

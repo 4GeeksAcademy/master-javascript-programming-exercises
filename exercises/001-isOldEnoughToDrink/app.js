abf37067219a54778013eb27f0d3a659f0f51deb
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 09:05:29 +0000

    exe1 done

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,3 +1,11 @@
 function isOldEnoughToDrink(age){
-    // your code here	
+    // your code here
+    if (age >= 21) {
+        return true
+    } 	else {
+        return false
+    }
 }
+
+let output = isOldEnoughToDrink(22);
+console.log(output);

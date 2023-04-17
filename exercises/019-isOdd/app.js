6aacaf6bcdef8dec4f5da4bda9ee387aef33f9b0
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 14:06:38 +0200

    Update app.js

--- exercises/019-isOdd/app.js
+++ exercises/019-isOdd/app.js
@@ -1,1 +1,12 @@
 // Write your function here
+function isOdd(num1) {
+    // your code here
+    if (num1 %2 != 0){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  let output = isOdd(9);
+console.log(output); // --> false

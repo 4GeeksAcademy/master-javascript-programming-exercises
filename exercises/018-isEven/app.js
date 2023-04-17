a0b5a7df29a0532f4d8a84dbe322b49e0ec6869b
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 14:05:32 +0200

    Update app.js

--- exercises/018-isEven/app.js
+++ exercises/018-isEven/app.js
@@ -1,1 +1,12 @@
 // Write your function here
+function isEven(num1) {
+    // your code here
+    if (num1 %2 == 0){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  let output = isEven(11);
+console.log(output); // --> false

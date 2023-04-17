28ee104f69d696c44edc3adaa114113b3b3ddad8
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 14:18:47 +0200

    Update app.js

--- exercises/023-isOddLength/app.js
+++ exercises/023-isOddLength/app.js
@@ -1,3 +1,11 @@
 function isOddLength(word){
-    
+    if (word.length %2 != 0){
+        return true
+      } else {
+        return false
+      }
 }
+
+
+let output = isOddLength('special');
+console.log(output); // --> true

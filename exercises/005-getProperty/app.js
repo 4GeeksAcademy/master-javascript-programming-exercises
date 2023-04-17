38cbde9b546d8c141b1f86918437c6942f7b0b72
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 13:35:07 +0200

    Update app.js

--- exercises/005-getProperty/app.js
+++ exercises/005-getProperty/app.js
@@ -1,3 +1,12 @@
 function getProperty(obj, key) {
   // your code here
-}
+  let property = obj[key];
+  return property;
+};
+
+let car = {
+  model: 'Toyota'
+};
+
+let output = getProperty(car, 'model');
+console.log(output);

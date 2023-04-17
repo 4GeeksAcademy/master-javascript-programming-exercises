99c89238fc4c00af835af28e01d777f4d7a951d5
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 14:27:07 +0200

    Update app.js

--- exercises/027-computeAreaOfATriangle/app.js
+++ exercises/027-computeAreaOfATriangle/app.js
@@ -1,2 +1,11 @@
-// Write your function here
+/// AOT = ½ (b × h)
+
+function computeAreaOfATriangle(base,height){
+    return 0.5 *(base * height)
+}
+
+
+let output = computeAreaOfATriangle(4, 6);
+console.log(output); // --> 12
+
 

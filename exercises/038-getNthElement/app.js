c022d785e24e3a15edcd4c8183c4298bb7111c9d
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 15:57:07 +0200

    Update app.js

--- exercises/038-getNthElement/app.js
+++ exercises/038-getNthElement/app.js
@@ -1,4 +1,12 @@
 function getNthElement(array, n) {
   // Add your code after this line
+  if (array.length === 0) {
+    return undefined
+  } else {
+    return array[n]
+  }
   
 }
+
+let output = getNthElement([1, 3, 5], 1);
+console.log(output); // --> 3

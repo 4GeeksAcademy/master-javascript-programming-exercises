74533ed780d24cdbe61a25a080678a2b2e0a18c5
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 13:54:45 +0200

    Update app.js

--- exercises/011-getLengthOfTwoWords/app.js
+++ exercises/011-getLengthOfTwoWords/app.js
@@ -1,3 +1,7 @@
 function getLengthOfTwoWords(word1, word2) {
   // your code here
+  return word1.length + word2.length
 }
+
+let output = getLengthOfTwoWords('some', 'words');
+console.log(output); // --> 9

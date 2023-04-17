b344f2d8eede089cefe9c199935503d1c7e6c9d3
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 15:53:09 +0200

    Update app.js

--- exercises/037-addArrayProperty/app.js
+++ exercises/037-addArrayProperty/app.js
@@ -1,4 +1,9 @@
 function addArrayProperty(obj, key, arr) {
   // Add your code after this line
-  
+  obj[key]=arr
+  return obj
 }
+let myObj = {};
+let myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty); // --> [1, 3]

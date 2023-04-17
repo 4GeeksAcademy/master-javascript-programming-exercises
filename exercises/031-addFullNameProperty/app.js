6b135731d65f29fdda5333c4eefbaf424eac5e32
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 15:39:30 +0200

    Update app.js

--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,4 +1,15 @@
-function addFullNameProperty(obj) {
-  // Add your code after this line
-  
-}
+// function
+function addFullNameProperty(obj){
+	obj['fullName'] = obj['firstName'] +' '+ obj['lastName']
+	return obj
+};
+
+let person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+
+addFullNameProperty(person);
+//console.log(person['firstName']); // --> 'Jade Smith'
+// console.log(person.firstName); // --> 'Jade Smith'
+console.log(person.fullName); // --> 'Jade Smith'

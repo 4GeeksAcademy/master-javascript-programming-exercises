b9c4bebb29f85c14881b98d4ed3512153e6ad67a
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 14:00:05 +0200

    Update app.js

--- exercises/014-equalsTen/app.js
+++ exercises/014-equalsTen/app.js
@@ -1,3 +1,11 @@
 function equalsTen(num) {
   // your code here
-}
+    if (num === 10){
+      return true
+    } else {
+      return false
+    }
+  }
+  let output = equalsTen(9);
+  console.log(output); // --> false
+  

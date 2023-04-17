304e395bea0223292f47d1d500c552af56f255e4
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-17 14:01:38 +0200

    Update app.js

--- exercises/015-isLessThan/app.js
+++ exercises/015-isLessThan/app.js
@@ -1,3 +1,11 @@
 function isLessThan(num1, num2) {
   // your code here
+  if (num1 > num2){
+    return true
+  } else {
+    return false
+  }
 }
+
+let output = isLessThan(9, 4);
+console.log(output); // --> true

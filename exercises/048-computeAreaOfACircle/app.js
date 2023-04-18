2399234baae331345f5b0f27f8528801b1b80328
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-18 16:24:12 +0200

    Update app.js

--- exercises/048-computeAreaOfACircle/app.js
+++ exercises/048-computeAreaOfACircle/app.js
@@ -1,3 +1,7 @@
 function computeAreaOfACircle(radius) {
     // your code here
+    return Math.PI * Math.pow(radius, 2)
 }
+
+let output = computeAreaOfACircle(4);
+console.log(output); // --> 50.26548245743669

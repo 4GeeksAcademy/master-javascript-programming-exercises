7e4fd6074c6286f40c7325f7c15d210f13dc13c0
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-18 16:20:39 +0200

    Update app.js

--- exercises/047-computePerimeterOfACircle/app.js
+++ exercises/047-computePerimeterOfACircle/app.js
@@ -1,4 +1,6 @@
 function computePerimeterOfACircle(radius) {
   // your code here
-
+  return 2*radius*Math.PI
 }
+let output = computePerimeterOfACircle(4);
+console.log(output); // --> 25.132741228718345

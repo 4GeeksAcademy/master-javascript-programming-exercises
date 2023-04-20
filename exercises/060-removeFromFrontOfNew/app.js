832b6cae7b8f0912c195ac96002a8076cc7d56ec
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-20 20:57:27 +0000

    gt

--- exercises/060-removeFromFrontOfNew/app.js
+++ exercises/060-removeFromFrontOfNew/app.js
@@ -1,4 +1,10 @@
 function removeFromFrontOfNew(arr) {
     // your code here
-    
+    return new_array = arr.filter((value,index, arr) => index > 0)   
 }
+
+//let arr = [1, 2, 3];
+let arr = [9, 21, 74,33];
+let output = removeFromFrontOfNew(arr);
+console.log(output); // --> [2, 3]
+console.log(arr); // --> [1, 2, 3]
--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,4 +1,13 @@
 function countCharacter(str, char) {
     // your code here
-    
+    charcount = 0
+    for (i in str){
+        if (i === char){
+            charcount = charcount + 1 
+        }
+    }
+    return charcount
 }
+
+let output = countCharacter('I am a hacker', 'a');
+console.log(output); // --> 3

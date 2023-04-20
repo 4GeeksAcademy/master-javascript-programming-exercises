9bc7ed1f44d18e1ace66dac75e503201684f18b7
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-20 19:41:14 +0000

    d

--- exercises/054-getElementsAfter/app.js
+++ exercises/054-getElementsAfter/app.js
@@ -1,4 +1,8 @@
 function getElementsAfter(array, n) {
   // your code here
+  return array.slice(n+1)
   
 }
+
+let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2); 
+console.log(output); // --> ['d', 'e']

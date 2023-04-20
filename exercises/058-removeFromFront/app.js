d0f76f47a0da8d9efe8010b14689763fe03f26f0
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-20 20:04:38 +0000

    d2

--- exercises/058-removeFromFront/app.js
+++ exercises/058-removeFromFront/app.js
@@ -1,4 +1,7 @@
 function removeFromFront(arr) {
     // your code here
-    
+    arr.shift()
+    return arr;
 }
+let output = removeFromFront([1, 2, 3]);
+console.log(output); // --> [2, 3]
--- exercises/059-removeFromBackOfNew/app.js
+++ exercises/059-removeFromBackOfNew/app.js
@@ -1,4 +1,10 @@
 function removeFromBackOfNew(arr) {
   // your code here
-  
+  new_array = arr.filter(arr)
+  return new_array
 }
+
+let arr = [1, 2, 3];
+let output = removeFromBackOfNew(arr);
+console.log(output); // --> [1, 2]
+console.log(arr); // --> [1, 2, 3]

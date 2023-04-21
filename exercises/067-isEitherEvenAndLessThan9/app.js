0ea7ca5326ac4e573771b48e1db81924cf668745
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-21 23:07:44 +0000

    d67

--- exercises/067-isEitherEvenAndLessThan9/app.js
+++ exercises/067-isEitherEvenAndLessThan9/app.js
@@ -1,4 +1,14 @@
 function isEitherEvenAndLessThan9(num1, num2) {
     // your code here
-
+    if (num1 < 9 && num2 < 9 && (num1 % 2 ==0||num2 % 2 ==0) ){
+        return true
+    } else {
+        return false
+    }
 }
+
+let output1 = isEitherEvenAndLessThan9(2, 4);
+console.log(output1); // --> true
+
+let output2 = isEitherEvenAndLessThan9(72, 2);
+console.log(output2); // --> false

2f22aeb9a66af7369c5c6b0b00ad2ce4a30f3c72
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-21 15:22:41 +0000

    dd

--- exercises/063-getAllWords/app.js
+++ exercises/063-getAllWords/app.js
@@ -1,4 +1,41 @@
+// get space
+// slice
+// push
 function getAllWords(str) {
     // your code here
+    charcount =0
+    for (var i = 0; i < str.length; i++){
+        if (str[i] === ' '){
+            charcount = charcount + 1   
+        }
+    }
     
+
+    let words = []
+    let todo = charcount +1
+    while (todo > 0){
+        spacePosition = str.search(' ')
+        word = str.slice(0,spacePosition)
+        words.push(word)
+        str = str.slice(spacePosition+1,)
+        todo = todo -1
+        
+        if (todo ==1){
+            word = str
+            words.push(word)
+            todo = 0
+        }
+        
+        
+ 
+
+    }
+    return words
+
 }
+
+let output = getAllWords('Radagast the Brown');
+//let output = getAllWords('the Brown');
+//let output2 = 'theBrown';
+console.log(output); // --> ['Radagast', 'the', 'Brown']
+//console.log(output2.search(' ')); // --> ['Radagast', 'the', 'Brown']

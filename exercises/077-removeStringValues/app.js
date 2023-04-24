03781e8e76fc4836c8a020c040e03c9cddceed00
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-24 10:06:59 +0000

    d77

--- exercises/077-removeStringValues/app.js
+++ exercises/077-removeStringValues/app.js
@@ -5,7 +5,14 @@ let obj = {
 
 function removeStringValues(obj) {
     // your code here
+    for (const key in obj){
+        if(typeof obj[key] === "string"){
+            delete obj[key]
     
+        }
+    }
+    
+    return obj
 }
 
 removeStringValues(obj);

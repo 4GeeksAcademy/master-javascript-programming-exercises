dfdab3208bc1b57965284ce78766fdade16b43ef
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-24 10:25:53 +0000

    p80

--- exercises/080-getAverageOfElementsAtProperty/app.js
+++ exercises/080-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,17 @@
 // Write your function here
+function getAverageOfElementsAtProperty(obj, key) {
+    // your code here
+    // get values
+    // get average
+    
+    
+  }
+
+
+
+let obj = {
+    key: [1, 2, 3]
+  };
+  let output = getAverageOfElementsAtProperty(obj, 'key');
+  console.log(output); // --> 2
+  

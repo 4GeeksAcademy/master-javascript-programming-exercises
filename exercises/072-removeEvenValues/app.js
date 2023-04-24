36953240f56e872735454f7813c37448e25d89f2
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-24 09:46:02 +0000

    d71

--- exercises/072-removeEvenValues/app.js
+++ exercises/072-removeEvenValues/app.js
@@ -1,4 +1,25 @@
 function removeEvenValues(obj) {
     // your code here
+    // check type
+    // check even
+    // del if necessary
+
+    for (const key in obj){
+        if(typeof obj[key] === "number"  && obj[key]%2 == 0 ){
+            delete obj[key]
+    
+        }
+    }
+    
+    return obj
+
     
 }
+
+let obj = {
+    a: 2,
+    b: 3,
+    c: 4
+  };
+  removeEvenValues(obj);
+  console.log(obj);

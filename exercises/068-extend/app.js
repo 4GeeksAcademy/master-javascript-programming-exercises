68b54424031af00bf999dad9fd179bfe1395b752
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-24 09:05:09 +0000

    d68

--- exercises/068-extend/app.js
+++ exercises/068-extend/app.js
@@ -9,5 +9,20 @@ let obj2 = {
 
 function extend(obj1, obj2) {
     // your code here
+    k1 = Object.keys(obj1)
+    k2 = Object.keys(obj2)
+    nck = k2.filter(x => !k1.includes(x))
+    
+    for (i in nck){
+        obj1[nck[i]]=obj2[nck[i]]
+    }
+    return obj1
+
 
 }
+
+extend(obj1, obj2);
+
+console.log(obj1); // --> {a: 1, b: 2, c: 3}
+console.log(obj2); // --> {b: 4, c: 3}
+console.log(extend(obj1, obj2));

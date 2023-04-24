3e2bb6c69e01cce99d3c84a81e4619fccf431c02
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-24 09:27:14 +0000

    d71

--- exercises/069-removeNumbersLargerThan/app.js
+++ exercises/069-removeNumbersLargerThan/app.js
@@ -3,7 +3,25 @@ let obj = {
     b: 2,
     c: 'montana'
 }
+
+console.log(obj); // --> { b: 2, c: 'montana' }
+
 function removeNumbersLargerThan(num, obj) {
     // your code here
+    // get type number
+    // compare and del if necessary
+
+    for (const key in obj){
+        if(typeof obj[key] === "number" && obj[key] > num ){
+            delete obj[key]
+
+        }
+    }
+
+    return obj
     
 }
+
+removeNumbersLargerThan(5, obj);
+console.log(obj); // --> { b: 2, c: 'montana' }
+//console.log(removeNumbersLargerThan(5, obj)); 

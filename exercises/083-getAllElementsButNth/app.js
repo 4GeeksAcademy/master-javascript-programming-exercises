a7a326881ae48b8056e63d9f84db1451c3cecb65
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-29 23:49:55 +0000

    d84

--- exercises/083-getAllElementsButNth/app.js
+++ exercises/083-getAllElementsButNth/app.js
@@ -1,6 +1,7 @@
 function getAllElementsButNth(array, n) {
     // Write your function here
-
+    array.splice(n, 1)
+    return array
 }
 let output = getAllElementsButNth(['a', 'b', 'c'], 1);
 console.log(output); // --> ['a', 'c']

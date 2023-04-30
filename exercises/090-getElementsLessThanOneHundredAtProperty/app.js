e741c188ea7d9054a2bf8be153828904e6a885d2
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-30 00:33:35 +0000

    d90

--- exercises/090-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/090-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,24 @@
 // Write your function here
+function getElementsLessThan100AtProperty(obj,key){
+    my_array = []
+    //Object.keys(obj)
+    if (obj[key] && (typeof(obj[key])==="object") && (Array.isArray(obj[key]))){
+        obj[key].forEach((e)=>{
+            if(e < 100){
+                my_array.push(e)
+            }
+        })
+    }
+
+    
+
+    return my_array
+    //return Object.keys(obj)
+}
+
+let obj = {
+    key: [1000, 20, 50, 500]
+  };
+  let output = getElementsLessThan100AtProperty(obj, 'key');
+  console.log(output); // --> [20, 50]
+  
--- exercises/091-countAllCharacters/app.js
+++ exercises/091-countAllCharacters/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function countAllCharacters(text){
+    count_char = {}
+
+
+    return count_char
+}
+
+let output = countAllCharacters('banana');
+console.log(output); // --> {b: 1, a: 3, n: 2}

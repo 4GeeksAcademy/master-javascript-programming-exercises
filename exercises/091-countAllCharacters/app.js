d61cf838197d58be119739d6d872320542701caf
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-05-02 13:29:17 +0000

    d91

--- exercises/091-countAllCharacters/app.js
+++ exercises/091-countAllCharacters/app.js
@@ -1,6 +1,29 @@
 // Write your function here
+// get letter
+// check if exist
+// if not exist value 1
+// if exist valua +1
+
+//for(let i; i<text.length;i++){
+//    if(count_char[text[i]]){
+//       count_char[text[i]]+ 
+//    } else {
+//        count_char[text[i]]=1
+//    }
+//}
+
 function countAllCharacters(text){
     count_char = {}
+    for(let i =0; i< text.length;i++){
+        if(count_char[text[i]]){
+            count_char[text[i]] += 1
+        } else {
+            count_char[text[i]] = 1
+        }
+
+        
+
+    }
 
 
     return count_char

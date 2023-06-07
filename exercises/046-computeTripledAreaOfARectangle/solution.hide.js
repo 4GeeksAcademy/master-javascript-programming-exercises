230f248ddbebd7fedf4ff92a3feb0e920b015d69
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 23:10:52 +0200

    Update solution.hide.js

--- exercises/046-computeTripledAreaOfARectangle/solution.hide.js
+++ exercises/046-computeTripledAreaOfARectangle/solution.hide.js
@@ -1,1 +1,9 @@
-// (a * b) * 3
+function computeTripledAreaOfARectangle(length, width) {
+  // your code here
+  let area = length * width;
+  let tripledArea = area * 3;
+  return tripledArea;
+}
+
+let output = computeTripledAreaOfARectangle(2, 4);
+console.log(output); // --> 24

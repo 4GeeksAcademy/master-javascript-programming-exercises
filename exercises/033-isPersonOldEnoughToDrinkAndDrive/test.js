2a8113377401c8163c151f409b05f9debdc00f95
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 20:18:40 +0200

    Update test.js

--- exercises/033-isPersonOldEnoughToDrinkAndDrive/test.js
+++ exercises/033-isPersonOldEnoughToDrinkAndDrive/test.js
@@ -9,14 +9,14 @@ test('Function isPersonOldEnoughToDrinkAndDrive must return something', () => {
     expect(isPersonOldEnoughToDrinkAndDrive({})).not.toBe(undefined);
 });
 
-test ('Function should return a false if age less than 21', () => {
+test ('Function should return false if age is less than 21', () => {
     expect(isPersonOldEnoughToDrinkAndDrive({ age: 20 })).toBe(false);
 })
-test ('Function should return a true if age greater than or equal than 21', () => {
+test ('Function should return true if age is greater than or equal to 21', () => {
     expect(isPersonOldEnoughToDrinkAndDrive({ age: 21 })).toBe(true);
     
 })
-test ('Function should return a true if age greater than or equal than 23', () => {
+test ('Function should return true if age is greater than or equal to 23', () => {
     expect(isPersonOldEnoughToDrinkAndDrive({ age: 23 })).toBe(true);
     
 })

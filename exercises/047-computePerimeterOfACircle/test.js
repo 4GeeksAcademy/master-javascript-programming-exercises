bcad16e3f5614b707a5cd6da99c2b4f64121c9a6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 23:15:13 +0200

    Update test.js

--- exercises/047-computePerimeterOfACircle/test.js
+++ exercises/047-computePerimeterOfACircle/test.js
@@ -11,12 +11,12 @@ test('Function computePerimeterOfACircle must return a number', () => {
     expect(typeof computePerimeterOfACircle(1)).toBe("number");
 });
 
-test('Must return the perimeter of a circle. Testing with 4', () => {    
+test('Function must return the perimeter of a circle. Testing with 4', () => {    
     let output = computePerimeterOfACircle(4)
     expect(output).toBe(25.132741228718345)
 })
 
-test('Must return the perimeter of a circle. Testing with 33', () => {
+test('Function must return the perimeter of a circle. Testing with 33', () => {
     let output = computePerimeterOfACircle(33)
     expect(output).toBe(207.34511513692635)
 })

caa7bbc03623b0c453589cc328ef00882bd607c8
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 22:35:01 +0200

    Update app.js

--- exercises/044-computePerimeterOfARectangle/app.js
+++ exercises/044-computePerimeterOfARectangle/app.js
@@ -1,5 +1,6 @@
 function computePerimeterOfARectangle(length, width) {
   // your code here
 }
+
 let output = computePerimeterOfARectangle(5, 2);
-console.log(output);
+console.log(output); // --> 14

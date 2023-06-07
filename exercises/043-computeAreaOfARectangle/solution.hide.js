60c37ffe6d966f67c16a2100ba4983ec19b688df
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 22:18:05 +0200

    Create solution.hide.js

---
+++ exercises/043-computeAreaOfARectangle/solution.hide.js
@@ -0,0 +1,8 @@
+function computeAreaOfARectangle(length, width) {
+  // your code here
+  let area = length * width;
+  return area;
+}
+
+let output = computeAreaOfARectangle(10, 18);
+console.log(output) // --> 180

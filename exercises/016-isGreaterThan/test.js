21305e366f8802540e59243fbfac7bda6330c144
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 18:00:41 +0200

    Update test.js

--- exercises/016-isGreaterThan/test.js
+++ exercises/016-isGreaterThan/test.js
@@ -1,7 +1,7 @@
 const rewire = require ("rewire");
 const isGreaterThan = rewire ('./app.js').__get__("isGreaterThan");
 
-test ('Create a function isGreaterThan', () => {
+test ('Create the function isGreaterThan', () => {
     expect((isGreaterThan)).toBeTruthy();
 })
 
@@ -13,10 +13,10 @@ test ('Your function must return a boolean (true or false)', () => {
     expect(typeof(isGreaterThan(9, 6))).toBe("boolean");
 })
 
-test ('The function should return true if num2 is bigger than num1', () => {
+test ('The function should return true if num2 is greater than num1', () => {
     expect(isGreaterThan(9, 12)).toBe(true);
 })
-test ('The function should return true if num2 is bigger than num1', () => {
+test ('The function should return true if num2 is greater than num1', () => {
     expect(isGreaterThan(1, 2)).toBe(true);
 })
 test ('The function should return false if num2 is less than num1', () => {

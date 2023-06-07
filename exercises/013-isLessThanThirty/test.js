8f7d0afbb3b12ea5d2790f6edabdcf8854bd9ac2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 16:30:33 +0200

    Update test.js

--- exercises/013-isLessThanThirty/test.js
+++ exercises/013-isLessThanThirty/test.js
@@ -1,17 +1,17 @@
 const rewire = require ("rewire");
 const isLessThan30 = rewire('./app.js').__get__('isLessThan30');
 
-test('The function isLessThan30 must exist.',()=>{
+test('The function isLessThan30 must exist',()=>{
     expect(isLessThan30).toBeTruthy();
 })
 
-test('The function isLessThan30 must return something.',()=>{
+test('The function isLessThan30 must return something',()=>{
     expect(isLessThan30()).not.toBe(undefined)
 })
 
-test ('function should return whether the given number is less than 30.', () => {
+test ('Function should return true if the given number is less than 30', () => {
     expect(isLessThan30(29)).toBe(true);
 })
-test ('function should return whether the given number is bigger than 30.', () => {
+test ('Function should return false if the given number is not less than 30', () => {
     expect(isLessThan30(30)).toBe(false);
 })

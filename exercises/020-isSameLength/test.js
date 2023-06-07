0779f9f1d55a21823a2eddce89862c634eec5954
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 18:23:56 +0200

    Update test.js

--- exercises/020-isSameLength/test.js
+++ exercises/020-isSameLength/test.js
@@ -8,22 +8,22 @@ test('Function isSameLength must return something', () => {
     expect(isSameLength('words', 'super')).not.toBe(undefined);
 });
 
-test('Function isSameLength must return boolean', () => {
+test('Function isSameLength must return a boolean', () => {
     expect(typeof(isSameLength("2","3"))).toBe("boolean");
 });
 
-test ('The function return true when the given words have the same length. Testing with "words", "super"', () => {
+test ('The function should return true when the given words have the same length. Testing with "words", "super"', () => {
     expect(isSameLength('words', 'super')).toBe(true);
 })
 
-test ('The function return true when the given words have the same length. Testing with "hello", "right"', () => {
+test ('The function should return true when the given words have the same length. Testing with "hello", "right"', () => {
     expect(isSameLength('hello', 'right')).toBe(true);
 })
 
-test ('The function return false when the given words haven\'t the same length. Testing with "words", "superior"', () => {
+test ('The function should return false when the given words are not the same length. Testing with "words", "superior"', () => {
     expect(isSameLength('words', 'superior')).toBe(false);
 })
 
-test ('The function return false when the given words haven\'t the same length. Testing with "when", "another"', () => {
+test ('The function should return false when the given words are not the same length. Testing with "when", "another"', () => {
     expect(isSameLength('when', 'another')).toBe(false);
 })

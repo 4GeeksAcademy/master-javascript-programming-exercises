d31d561f4def372bd8c9bc147e79182783973a88
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 20:40:05 +0200

    Update test.js

--- exercises/035-isPersonOldEnoughToVote/test.js
+++ exercises/035-isPersonOldEnoughToVote/test.js
@@ -9,20 +9,20 @@ test('Function isPersonOldEnoughToVote must return something', () => {
     expect(isPersonOldEnoughToVote({})).not.toBe(undefined);
 });
 
-test ('Function should return a false if age less than 18. Testing with 17', () => {
+test ('Function should return false if age is less than 18. Testing with 17', () => {
    
     expect(isPersonOldEnoughToVote({ age: 17 })).toBe(false);
 })
-test ('Function should return a false if age less than 18. Testing with 5', () => {
+test ('Function should return false if age less than 18. Testing with 5', () => {
    
     expect(isPersonOldEnoughToVote({ age: 5 })).toBe(false);
 })
-test ('Function should return a true if age are greather than or equal to 18. Testing with 18', () => {
+test ('Function should return true if age is greater than or equal to 18. Testing with 18', () => {
   
     expect(isPersonOldEnoughToVote({ age: 18 })).toBe(true);
 
 })
-test ('Function should return a true if age are greather than or equal to 18. Testing with 20', () => {
+test ('Function should return true if age is greater than or equal to 18. Testing with 20', () => {
   
    
     expect(isPersonOldEnoughToVote({ age: 20 })).toBe(true);

d8653f7a03378b2e879b67357d049d2a9cad317b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 17:54:56 +0200

    Update test.js

--- exercises/015-isLessThan/test.js
+++ exercises/015-isLessThan/test.js
@@ -1,26 +1,26 @@
 const rewire = require ("rewire");
 const isLessThan = rewire('./app.js').__get__('isLessThan');
 
-test('The function isLessThan must exist.',()=>{
+test('The function isLessThan must exist',()=>{
     expect(isLessThan).toBeTruthy();
 })
-test('The function isLessThan should return something.',()=>{
+test('The function isLessThan should return something',()=>{
     expect(isLessThan(5, 6)).not.toBe(undefined);
 })
 
-test ('The function return true if num1 is bigger than num2.', () => {
+test ('The function returns true if num1 is bigger than num2', () => {
     const isLessThan = rewire ('./app.js').__get__("isLessThan");
     expect(isLessThan(9, 3)).toBe(true);
 })
-test ('The function return true if num1 is bigger than num2.', () => {
+test ('The function returns true if num1 is bigger than num2', () => {
     const isLessThan = rewire ('./app.js').__get__("isLessThan");
     expect(isLessThan(3, 2)).toBe(true);
 })
 
-test('The function return false if num1 is less than num2', ()=>{
+test('The function returns false if num1 is less than num2', ()=>{
     expect(isLessThan(6, 12)).toBe(false);
 })
 
-test('The function return false if num1 is less than num2', ()=>{
+test('The function returns false if num1 is less than num2', ()=>{
     expect(isLessThan(66, 84)).toBe(false);
 })

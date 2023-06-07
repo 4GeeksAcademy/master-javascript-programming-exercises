26d2175543c4a52db5b3152e628630813932e153
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-07 22:48:04 +0200

    Update solution.hide.js

--- exercises/045-computePerimeterOfATriangle/solution.hide.js
+++ exercises/045-computePerimeterOfATriangle/solution.hide.js
@@ -1,1 +1,8 @@
-// a+b+c
+function computePerimeterOfATriangle(side1, side2, side3) {
+  // your code here
+  let perimeter = side1 + side2 + side3;
+  return perimeter;
+}
+
+let output = computePerimeterOfATriangle(6, 4, 10);
+console.log(output); // --> 20

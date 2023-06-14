bc42177a583d83551ea3877f790e87616c135292
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 21:21:28 +0200

    Update test.js

--- exercises/078-convertDoubleSpaceToSingle/test.js
+++ exercises/078-convertDoubleSpaceToSingle/test.js
@@ -13,17 +13,17 @@ test('Function convertDoubleSpaceToSingle must return a string', () => {
     expect(typeof convertDoubleSpaceToSingle("test  test")).toBe("string");
 });
 
-test('The function must return all the double spaces converted to single spaces.', () => {
+test('The function must return all the double spaces converted to single spaces', () => {
     let output = convertDoubleSpaceToSingle("my  cat  has  broken,  says  woof");
     expect(output).toEqual("my cat has broken, says woof");
 });
 
-test('The function must return all the double spaces converted to single spaces.', () => {
+test('The function must return all the double spaces converted to single spaces. Testing with different values', () => {
     let output = convertDoubleSpaceToSingle("string  with  double  spaces");
     expect(output).toEqual("string with double spaces");
 });
 
-test('The function must return all the double spaces converted to single spaces.', () => {
+test('The function must return all the double spaces converted to single spaces. Testing with different values', () => {
     let output = convertDoubleSpaceToSingle("string  with double spaces");
     expect(output).toEqual("string with double spaces");
 });

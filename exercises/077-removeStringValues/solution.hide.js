80713e05760f1d043624f4235b21639c6ac49df7
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 20:58:34 +0200

    Create solution.hide.js

---
+++ exercises/077-removeStringValues/solution.hide.js
@@ -0,0 +1,17 @@
+function removeStringValues(obj) {
+    // your code here
+    for (let key in obj) {
+        if (typeof obj[key] === 'string') {
+          delete obj[key];
+        }
+      }
+    return obj;
+}
+
+let obj = {
+    name: 'Sam',
+    age: 20
+}
+
+removeStringValues(obj);
+console.log(obj); // { age: 20 }

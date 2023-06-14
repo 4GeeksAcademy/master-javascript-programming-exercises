ffe27e56de1fc6b0b06e71631c8e84b27bdc4143
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 20:26:50 +0200

    Update app.js

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,14 +1,16 @@
 let obj = {
-    person:"Rafael",
-    lastName:"Leao",
+    firstName: "Rafael",
+    lastName: "Leao",
     country: "Italy",
     city: "Milan",
     phoneNum: "123-456-789",
-    jerseyNum: 27,
+    jerseyNum: 27
 }
 
 function countNumberOfKeys(obj) {
     // your code here
    
 }
-console.log(countNumberOfKeys(obj))
+
+let output = countNumberOfKeys(obj);
+console.log(output); // --> 3

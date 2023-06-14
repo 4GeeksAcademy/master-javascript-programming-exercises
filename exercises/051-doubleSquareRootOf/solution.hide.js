bf04f14f4218ccfff7d3c6afef81da9f2550fefc
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 03:21:40 +0200

    Create solution.hide.js

---
+++ exercises/051-doubleSquareRootOf/solution.hide.js
@@ -0,0 +1,9 @@
+function doubleSquareRootOf(num) {
+  // your code here
+  let squareRoot = Math.sqrt(num);
+  let result = squareRoot * 2;
+  return result;
+}
+
+let output = doubleSquareRootOf(121);
+console.log(output); // --> 22

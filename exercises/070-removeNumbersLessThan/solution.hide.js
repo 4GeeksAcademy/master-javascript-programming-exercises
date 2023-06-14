e8da2f787a91e1cb1ade6947ab827207cf0b6df9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 19:35:57 +0200

    Create solution.hide.js

---
+++ exercises/070-removeNumbersLessThan/solution.hide.js
@@ -0,0 +1,15 @@
+let obj = {
+    a: 8,
+    b: 2,
+    c: 'montana'
+}
+
+function removeNumbersLessThan(num, obj) {
+    // your code here
+    for (let key in obj) {
+        if (typeof obj[key] === 'number' && obj[key] < num) {
+          delete obj[key];
+        }
+      }
+    return obj;
+}

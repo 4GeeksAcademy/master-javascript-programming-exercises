75dc31a8d0c9f30d08880884be361c50f4cf75fb
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 15:31:11 +0200

    Update solution.hide.js

--- exercises/056-getAllElementsButFirst/solution.hide.js
+++ exercises/056-getAllElementsButFirst/solution.hide.js
@@ -7,3 +7,4 @@ function getAllElementsButFirst(array) {
 let input = [1, 2, 3, 4];
 let output = getAllElementsButFirst(input);
 console.log(output); // --> [2, 3, 4]
+console.log(input);  // --> [1, 2, 3, 4]

09222e607d41b580474eddea7ef2906d8f2dacf9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 20:51:05 +0200

    Update solution.hide.js

--- exercises/076-removeNumberValues/solution.hide.js
+++ exercises/076-removeNumberValues/solution.hide.js
@@ -1,9 +1,18 @@
-function removeArrayValues(obj) {
+function removeNumberValues(obj) {
     // your code here
     for (let key in obj) {
-        if (Array.isArray(obj[key])) {
+        if (typeof obj[key] === 'number') {
           delete obj[key];
         }
       }
     return obj;
 }
+
+let obj = {
+    a: 2,
+    b: 'remaining',
+    c: 4
+};
+
+removeNumberValues(obj);
+console.log(obj); // --> { b: 'remaining' }

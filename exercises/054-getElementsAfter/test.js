9d1725f5513d39fe34ed7aa8962c1d5019d54880
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 03:42:46 +0200

    Update test.js

--- exercises/054-getElementsAfter/test.js
+++ exercises/054-getElementsAfter/test.js
@@ -13,12 +13,12 @@ test('Function getElementsAfter must return a list', () => {
     expect(typeof getElementsAfter([1, 2],1)).toBe("object");
 });
 
-test('Function must return a new array with all the elements after the given index.', () => {
+test('Function must return a new array with all the elements after the given index', () => {
     let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2)
     expect(output).toEqual(['d', 'e'])
 })
 
-test('The function must not return the other elements of the array.', () => {
+test('The function must not return the other elements of the array', () => {
     let output = getElementsAfter(['you', 'can', 'do', 'it'], 1)
     expect(output).toEqual(['do', 'it'])
 })

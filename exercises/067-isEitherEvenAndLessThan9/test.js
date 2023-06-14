7dfccf93c55257c0ffbc8dcc0727fca1199a71f2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 18:06:39 +0200

    Update test.js

--- exercises/067-isEitherEvenAndLessThan9/test.js
+++ exercises/067-isEitherEvenAndLessThan9/test.js
@@ -14,32 +14,32 @@ test('Function "isEitherEvenAndLessThan9" must return a boolean', () => {
     expect(typeof isEitherEvenAndLessThan9(1,2)).toBe("boolean");
 });
 
-test('The function must return true if at least one number is even and both are less than 9.', () => {
+test('The function must return true if at least one number is even and both are less than 9. Testing with (3,4)', () => {
     let output = isEitherEvenAndLessThan9(3, 4);
     expect(output).toBe(true);
 });
 
-test('The function must return true if both numbers are even and both are less than 9.', () => {
+test('The function must return true if at least one number is even and both are less than 9. Testing with (2,8)', () => {
     let output = isEitherEvenAndLessThan9(2, 8);
     expect(output).toBe(true);
 });
 
-test('The function must return true if both numbers are 0', () => {
+test('The function must return true if at least one number is even and both are less than 9. Testing with (0,0)', () => {
     let output = isEitherEvenAndLessThan9(0, 0);
     expect(output).toBe(true);
 });
 
-test('The function must return false if both numbers are even but one is greater than 9.', () => {
+test('The function must return true if at least one number is even and both are less than 9. Testing with (72,2)', () => {
     let output = isEitherEvenAndLessThan9(72, 2);
     expect(output).toBe(false);
 });
 
-test('The function must return false if both numbers are even and both are greater than 9.', () => {
+test('The function must return true if at least one number is even and both are less than 9. Testing with (72,10)', () => {
     let output = isEitherEvenAndLessThan9(72, 10);
     expect(output).toBe(false);
 });
 
-test('The function must return false if both numbers are not even and both are less than 9.', () => {
+test('The function must return true if at least one number is even and both are less than 9. Testing with (9,5)', () => {
     let output = isEitherEvenAndLessThan9(9, 5);
     expect(output).toBe(false);
 });

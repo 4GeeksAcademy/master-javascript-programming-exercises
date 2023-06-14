e4b71f6281ecc076061aec26f547e636fe4ab222
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 21:20:18 +0200

    Update app.js

--- exercises/078-convertDoubleSpaceToSingle/app.js
+++ exercises/078-convertDoubleSpaceToSingle/app.js
@@ -2,5 +2,6 @@ function convertDoubleSpaceToSingle(str) {
     // your code here
     
 }
+
 let output = convertDoubleSpaceToSingle("string  with  double  spaces");
 console.log(output); // --> "string with double spaces"

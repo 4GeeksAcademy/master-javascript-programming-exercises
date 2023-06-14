d2ec2fa1b93d75f2e69d557ee0744a0dd39d91eb
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 19:55:24 +0200

    Update test.js

--- exercises/071-removeStringValuesLongerThan/test.js
+++ exercises/071-removeStringValuesLongerThan/test.js
@@ -7,7 +7,7 @@ test('Function removeStringValuesLongerThan must exist', () => {
   expect(removeStringValuesLongerThan).not.toBe(undefined);
 });
 
-test('The function must eliminate the property whose values are strings longer than the given number.', () => {
+test('The function must eliminate the property whose values are strings longer than the given number', () => {
   let obj = {
     name: 'Tea',
     price: 5,
@@ -18,7 +18,7 @@ test('The function must eliminate the property whose values are strings longer t
   expect(obj).toEqual({ name: 'Tea', price: 5, type: 'Mint' });
 });
 
-test('The function must eliminate the property whose values are strings longer than the given number.', () => {
+test('The function must eliminate the property whose values are strings longer than the given number. Testing with different values', () => {
   const removeStringValuesLongerThan = rewire('./app.js').__get__(
     'removeStringValuesLongerThan'
   );
@@ -31,7 +31,7 @@ test('The function must eliminate the property whose values are strings longer t
   expect(obj).toEqual({ age: 20, location: 'Texas' });
 });
 
-test('The function must return the same object if no property is longer than the given number.', () => {
+test('The function must return the same object if no property is longer than the given number', () => {
   var obj = {
     name: 'Tea',
     price: 5,

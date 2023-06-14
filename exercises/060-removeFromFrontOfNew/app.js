97aa5e63b434eb6e329bc3326d9ba4a0b155b4e8
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 16:21:17 +0200

    Update app.js

--- exercises/060-removeFromFrontOfNew/app.js
+++ exercises/060-removeFromFrontOfNew/app.js
@@ -2,3 +2,8 @@ function removeFromFrontOfNew(arr) {
     // your code here
     
 }
+
+let arr = [1, 2, 3];
+let output = removeFromFrontOfNew(arr);
+console.log(output); // --> [2, 3]
+console.log(arr); // --> [1, 2, 3]

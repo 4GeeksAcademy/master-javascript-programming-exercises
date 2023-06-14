644c020eec21fd4e9ebc16b26156ae847a400ebb
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 20:49:35 +0200

    Update app.js

--- exercises/076-removeNumberValues/app.js
+++ exercises/076-removeNumberValues/app.js
@@ -1,12 +1,13 @@
+function removeNumberValues(obj) {
+    // your code here
+    
+}
+
 let obj = {
     a: 2,
     b: 'remaining',
     c: 4
 };
-function removeNumberValues(obj) {
-    // your code here
-    
-}
 
 removeNumberValues(obj);
 console.log(obj); // --> { b: 'remaining' }

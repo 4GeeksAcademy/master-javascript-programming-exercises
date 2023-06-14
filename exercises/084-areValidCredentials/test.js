dba838f77e08bd37ccfc4d6d9af7b2b226589317
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 22:52:32 +0200

    Update test.js

--- exercises/084-areValidCredentials/test.js
+++ exercises/084-areValidCredentials/test.js
@@ -13,27 +13,27 @@ test('Function areValidCredentials must return a boolean', () => {
   expect(typeof areValidCredentials('test', 'testpassword')).toBe('boolean');
 });
 
-test('The name should have more than 3 characters. and, the password at least 8 characters long. Testing with different values (1/5)', () => {
+test('The name should have more than 3 characters, and the password at least 8 characters long. Testing with different values (1/5)', () => {
   let output = areValidCredentials('Ritu', 'mylongpassword');
   expect(output).toBe(true);
 });
 
-test('The name should have more than 3 characters. and, the password at least 8 characters long. Testing with different values (2/5)', () => {
+test('The name should have more than 3 characters, and the password at least 8 characters long. Testing with different values (2/5)', () => {
   let output = areValidCredentials('Johnathan', '123456789J');
   expect(output).toBe(true);
 });
 
-test('The name should have more than 3 characters. and, the password at least 8 characters long. Testing with different values (3/5)', () => {
+test('The name should have more than 3 characters, and the password at least 8 characters long. Testing with different values (3/5)', () => {
   let output = areValidCredentials('ko', '123321');
   expect(output).toBe(false);
 });
 
-test('The name should have more than 3 characters. and, the password at least 8 characters long. Testing with different values (4/5)', () => {
+test('The name should have more than 3 characters, and the password at least 8 characters long. Testing with different values (4/5)', () => {
   let output = areValidCredentials('john', '123321');
   expect(output).toBe(false);
 });
 
-test('The name should have more than 3 characters. and, the password at least 8 characters long. Testing with different values (5/5)', () => {
+test('The name should have more than 3 characters, and the password at least 8 characters long. Testing with different values (5/5)', () => {
   let output = areValidCredentials('El', '123321890');
   expect(output).toBe(false);
 });

3c73ab64fb8e39c40a7e1f7f8196e768bb43ae26
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-14 02:38:25 +0200

    Create solution.hide.js

---
+++ exercises/048-computeAreaOfACircle/solution.hide.js
@@ -0,0 +1,8 @@
+function computeAreaOfACircle(radius) {
+    // your code here
+    let area = Math.PI * radius * radius;
+    return area;
+}
+
+let output = computeAreaOfACircle(4);
+console.log(output); // --> 50.26548245743669

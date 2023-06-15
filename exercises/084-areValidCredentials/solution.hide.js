3c32c36b5cfa2d0d8bfffa375cca3d0250693ec8
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2023-06-15 16:10:22 -0400

    Update solution.hide.js

--- exercises/084-areValidCredentials/solution.hide.js
+++ exercises/084-areValidCredentials/solution.hide.js
@@ -1,4 +1,4 @@
 // Write your function here
 function areValidCredentials(name, pass) {
-    return name.length > 3 && pass.length >= 8 ? true : false;
+    return name.length > 3 && pass.length >= 8;
 }

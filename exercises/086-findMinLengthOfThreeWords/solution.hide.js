107d913f8de04e53495acb3246e93ef9bd637b23
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-20 20:40:10 +0200

    Update solution.hide.js

--- exercises/086-findMinLengthOfThreeWords/solution.hide.js
+++ exercises/086-findMinLengthOfThreeWords/solution.hide.js
@@ -1,3 +1,4 @@
+// Write your function here
 function findMinLengthOfThreeWords(word1, word2, word3) {
     let minLength = Math.min(word1.length, word2.length, word3.length);
     return minLength;

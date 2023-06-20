97facd4dfc15fd1a42d2de1b383d1ea08ca08bde
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-20 20:38:22 +0200

    Update solution.hide.js

--- exercises/086-findMinLengthOfThreeWords/solution.hide.js
+++ exercises/086-findMinLengthOfThreeWords/solution.hide.js
@@ -1,9 +1,4 @@
-function findMinLengthOfThreeWords(a, b, c) {
-    if (a.length < b.length && a.length < c.length) {
-        return a.length
-    } else if (b.length < a.length && b.length < c.length) {
-        return b.length
-    } else {
-        return c.length
-    }
+function findMinLengthOfThreeWords(word1, word2, word3) {
+    let minLength = Math.min(word1.length, word2.length, word3.length);
+    return minLength;
 }

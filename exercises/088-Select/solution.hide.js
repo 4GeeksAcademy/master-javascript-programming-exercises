b4678bd00dbad673c626bfcef2f907ae704d2be9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-20 21:02:19 +0200

    Update solution.hide.js

--- exercises/088-Select/solution.hide.js
+++ exercises/088-Select/solution.hide.js
@@ -1,7 +1,7 @@
 // Write your function here
 function select(arr, obj) {
     let newObj = {};
-    arr.forEach((e, index) => {
+    arr.forEach((e) => {
         if(obj[e]) {
             newObj[e] = obj[e]
         }

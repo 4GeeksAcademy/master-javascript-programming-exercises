ab893228844c7437bd8c1df690c82f930a782f91
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 19:19:33 +0200

    Update app.js

--- exercises/110-getAllButLastElementOfProperty/app.js
+++ exercises/110-getAllButLastElementOfProperty/app.js
@@ -1,9 +1,10 @@
+function getAllButLastElementOfProperty(obj, key) {
+    // your code here
+    
+}
+
 let obj = {
   key: [1, 2, 3]
 };
-function getAllButLastElementOfProperty(obj, key) {
-    // your code here
-}
-
 let output = getAllButLastElementOfProperty(obj, 'key');
-console.log(output); // --> [1,2]
+console.log(output); // --> [1, 2]

caf27aa7e1253ba564bcbbc6affc898d84663754
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 23:04:22 +0200

    Update app.js

--- exercises/124-joinArraysOfArrays/app.js
+++ exercises/124-joinArraysOfArrays/app.js
@@ -1,5 +1,7 @@
 function joinArrayOfArrays(arr) {
   // your code here
+  
 }
+
 let output = joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']]);
 console.log(output); // --> [1, 4, true, false, 'x', 'y']

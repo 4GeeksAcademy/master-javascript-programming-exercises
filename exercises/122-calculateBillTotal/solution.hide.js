2f4c6467d1eeb2ae32b190ec3ebab820dd29f4cf
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 22:15:16 +0200

    Update solution.hide.js

--- exercises/122-calculateBillTotal/solution.hide.js
+++ exercises/122-calculateBillTotal/solution.hide.js
@@ -4,5 +4,6 @@ function calculateBillTotal(preTaxAndTipAmount) {
   let tip = preTaxAndTipAmount * 0.15;
   return preTaxAndTipAmount + tax + tip;
 }
+
 let output = calculateBillTotal(20);
 console.log(output); // --> 24.9

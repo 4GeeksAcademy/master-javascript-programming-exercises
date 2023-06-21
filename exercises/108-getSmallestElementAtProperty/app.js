61e3a0dd20c258931d1b4f4a36f30892051603b0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 18:52:42 +0200

    Update app.js

--- exercises/108-getSmallestElementAtProperty/app.js
+++ exercises/108-getSmallestElementAtProperty/app.js
@@ -1,8 +1,10 @@
+function getSmallestElementAtProperty(obj, key) {
+    // your code here
+  
+}
+
 let obj = {
   key: [2, 1, 5]
 };
-function getSmallestElementAtProperty(obj, key) {
-    // your code here
-}
 let output = getSmallestElementAtProperty(obj, 'key');
 console.log(output); // --> 1

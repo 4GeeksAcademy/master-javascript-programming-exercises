d917d6a35b1fad45e3b29acea6812c67f20895ca
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 17:15:29 +0200

    Update solution.hide.js

--- exercises/104-getOddElementsAtProperty/solution.hide.js
+++ exercises/104-getOddElementsAtProperty/solution.hide.js
@@ -1,16 +1,13 @@
+function getOddElementsAtProperty(obj, key) {
+  if (!obj.hasOwnProperty(key) || !Array.isArray(obj[key])) {
+    return [];
+  }
+
+  return obj[key].filter(num => num % 2 !== 0);
+}
+
 let obj = {
   key: [1, 2, 3, 4, 5]
 };
-
-function getOddElementsAtProperty(obj, key) {
-  // your code here
-  if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length < 1) return []
-  else {
-    let aux = []
-    obj[key].map(item => item % 2 != 0 ? aux.push(item) : null)
-    return aux
-  }
-}
-
 let output = getOddElementsAtProperty(obj, 'key');
 console.log(output); // --> [1, 3, 5]

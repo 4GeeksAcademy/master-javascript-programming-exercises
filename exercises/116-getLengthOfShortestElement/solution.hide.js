88d00d09a461fa4e4ab6983870edf70b6c847414
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 21:11:30 +0200

    Update solution.hide.js

--- exercises/116-getLengthOfShortestElement/solution.hide.js
+++ exercises/116-getLengthOfShortestElement/solution.hide.js
@@ -1,11 +1,13 @@
 function getLengthOfShortestElement(arr) {
-  // your code here
-  let aux = 9999999999;
-  if (arr.length < 1) return 0;
-  for (let e of arr) {
-    if (e.length < aux) aux = e.length;
-  }
-  return aux;
+    // your code here
+    if (arr.length < 1) return 0;
+
+    let aux = 9999999999;
+    for (let e of arr) {
+      if (e.length < aux) aux = e.length;
+    }
+    return aux;
 }
+
 let output = getLengthOfShortestElement(['one', 'two', 'three']);
 console.log(output); // --> 3

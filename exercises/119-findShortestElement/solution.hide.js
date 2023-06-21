d2654605e230ad9dda11787dda9761321c33653e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 21:46:58 +0200

    Update solution.hide.js

--- exercises/119-findShortestElement/solution.hide.js
+++ exercises/119-findShortestElement/solution.hide.js
@@ -1,7 +1,8 @@
 function findShortestElement(arr) {
   // your code here
-  let aux = '99999999999999999999999999999999999999999999999';
   if (arr.length < 1) return '';
+  
+  let aux = '999999999999999999999999999999';
   for (let e of arr) {
     if (e.length < aux.length) aux = e;
   }

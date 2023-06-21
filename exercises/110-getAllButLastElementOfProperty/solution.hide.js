946391956d750df3a28c20919ef7da76fe2535f9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 19:21:41 +0200

    Update solution.hide.js

--- exercises/110-getAllButLastElementOfProperty/solution.hide.js
+++ exercises/110-getAllButLastElementOfProperty/solution.hide.js
@@ -1,13 +1,11 @@
-let obj = {
-    key: [1, 2, 3]
-};
-
 function getAllButLastElementOfProperty(obj, key) {
     // your code here
     if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length < 1) return []
     else { obj[key].pop(); return obj[key] }
-
 }
 
+let obj = {
+    key: [1, 2, 3]
+};
 let output = getAllButLastElementOfProperty(obj, 'key');
-console.log(output); // --> [1,2]
+console.log(output); // --> [1, 2]

2ba032af35b23b3bc2f57364113bf83d3581c393
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 14:52:12 +0200

    Update test.js

--- exercises/097-keep/test.js
+++ exercises/097-keep/test.js
@@ -9,21 +9,21 @@ test('Function keep must return something', () => {
   expect(keep([1, 2, 3], 2)).not.toBe(undefined);
 });
 
-test('Function keep must return an array.', () => {
+test('Function keep must return an array', () => {
   expect(typeof keep([1, 2, 1], 1)).toBe('object');
 });
 
-test('The function must return only the elements that match the given keeper element.', () => {
+test('The function must return only the elements that match the given keeper element', () => {
   let output = keep([1, 2, 3, 2, 1], 2);
   expect(output).toEqual([2, 2]);
 });
 
-test('The function must return only the elements that match the given keeper element. Testing with different values.', () => {
+test('The function must return only the elements that match the given keeper element. Testing with different values', () => {
   let output = keep([1, 2, 1, 2, 1], 1);
   expect(output).toEqual([1, 1, 1]);
 });
 
-test('If no elements match, the function keep should return an empty array [].', () => {
+test('If no elements match, the function should return an empty array []', () => {
   let output = keep([1, 2, 1, 2, 1], 3);
   expect(output).toEqual([]);
 });

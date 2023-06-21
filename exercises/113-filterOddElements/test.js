c10cf42b49af36ed444acbe1eed8031bb95010d0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 20:11:30 +0200

    Update test.js

--- exercises/113-filterOddElements/test.js
+++ exercises/113-filterOddElements/test.js
@@ -11,10 +11,10 @@ test('Function filterOddElements must return an array', () => {
   expect(typeof filterOddElements([1, 2])).toBe('object');
 });
 
-test('Function must returns an array containing only the odd numbers of the given array.', () => {
+test('Function must return an array containing only the odd numbers of the given array', () => {
   expect(filterOddElements([3, 9, 12, 37])).toEqual([3, 9, 37]);
 });
 
-test('Function must returns an array containing only the odd numbers of the given array. Testing with example values.', () => {
+test('Function must return an array containing only the odd numbers of the given array. Testing with different values', () => {
   expect(filterOddElements([1, 2, 3, 4, 5])).toEqual([1, 3, 5]);
 });

2cf46e09aa1f9e794e80e19646137c69580642d9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 17:44:24 +0200

    Update app.js

--- exercises/106-getEvenElementsAtProperty/app.js
+++ exercises/106-getEvenElementsAtProperty/app.js
@@ -1,10 +1,10 @@
+function getEvenElementsAtProperty(obj, key) {
+    // your code here
+    
+}
 
 let obj = {
   key: [1000, 11, 50, 17]
 };
-
-function getEvenElementsAtProperty(obj, key) {
-    // your code here
-}
 let output = getEvenElementsAtProperty(obj, 'key');
 console.log(output); // --> [1000, 50]

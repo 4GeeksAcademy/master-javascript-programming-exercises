16c8a9005c2f5ebaaad0bfae00a9b7d049fc3aef
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 22:55:41 +0200

    Update solution.hide.js

--- exercises/123-getStringLength/solution.hide.js
+++ exercises/123-getStringLength/solution.hide.js
@@ -1,11 +1,25 @@
 function getStringLength(string) {
-  // your code here
-  let count = 0;
-  for (let char of string) {
-    count++;
+    // your code here
+    let count = 0;
+    for (let char of string) {
+      count++;
+    }
+    return count;
   }
-  return count;
+  
+  let output = getStringLength('hello');
+  console.log(output); // --> 5
+
+/*
+
+  Solution #2:
+
+function getStringLength(string) {
+  // your code here
+  return [...string].reduce((a) => a + 1, 0);
 }
 
 let output = getStringLength('hello');
 console.log(output); // --> 5
+
+*/

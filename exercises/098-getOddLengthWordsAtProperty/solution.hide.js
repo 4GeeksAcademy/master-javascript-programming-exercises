0fc69b53af49fdf472c3c4b19fb9a51e2dc4742b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 15:10:45 +0200

    Update solution.hide.js

--- exercises/098-getOddLengthWordsAtProperty/solution.hide.js
+++ exercises/098-getOddLengthWordsAtProperty/solution.hide.js
@@ -1,12 +1,12 @@
 // Write your function here
-function getOddLengthWordsAtProperty(obj, key){
-    if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length == 0){
+function getOddLengthWordsAtProperty(obj, key) {
+    if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length == 0) {
         return []
     }
 
     let newArr = [];
-    for(let i = 0; i < obj[key].length; i++){
-        if(obj[key][i].length % 2 !== 0){
+    for(let i = 0; i < obj[key].length; i++) {
+        if(obj[key][i].length % 2 !== 0) {
             newArr.push(obj[key][i])
         }
     }

4463c295236b6548ad6b4d51b955b505259bd64f
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 23:17:13 +0200

    Update app.js

--- exercises/125-getProductOfAllElementsAtProperty/app.js
+++ exercises/125-getProductOfAllElementsAtProperty/app.js
@@ -1,9 +1,10 @@
+function getProductOfAllElementsAtProperty(obj, key) {
+    // your code here
+    
+}
+
 let obj = {
   key: [1, 2, 3, 4]
 };
-
-function getProductOfAllElementsAtProperty(obj, key) {
-    // your code here
-}
 let output = getProductOfAllElementsAtProperty(obj, 'key');
 console.log(output); // --> 24

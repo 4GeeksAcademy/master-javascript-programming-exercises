35be9b59c11a79494fd28d79c41c12bf35b4b52e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 16:23:39 +0200

    Update solution.hide.js

--- exercises/102-filterOddLengthWords/solution.hide.js
+++ exercises/102-filterOddLengthWords/solution.hide.js
@@ -1,14 +1,6 @@
 function filterOddLengthWords(words) {
-  /*
-  Using filter method:
-  return words.filter((item) => item.length % 2 !== 0)
-  */
-  // your code here
-  let aux = []
-  words.map(w => {
-      w.length % 2 !== 0 ? aux.push(w) : null
-  })
-  return aux;
+    // your code here
+    return words.filter((item) => item.length % 2 !== 0)
 }
 
 let output = filterOddLengthWords(['there', 'it', 'is', 'now']);

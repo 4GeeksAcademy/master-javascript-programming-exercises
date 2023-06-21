2139de57f2e95664717d0bc049a76e335fe48d48
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 17:05:53 +0200

    Update solution.hide.js

--- exercises/103-getSquaredElementsAtProperty/solution.hide.js
+++ exercises/103-getSquaredElementsAtProperty/solution.hide.js
@@ -1,11 +1,14 @@
 function getSquaredElementsAtProperty(obj, key) {
-    // Your code here
-    if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length < 1) return []
-    else return obj[key].map(e => e * e);
+  // Your code here
+  if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length < 1) {
+    return []
+  }
+
+  return obj[key].map(e => e * e);
 }
 
 let obj = {
-    key: [2, 1, 5]
+  car: [2, 1, 5]
 };
-let output = getSquaredElementsAtProperty(obj, 'key');
+let output = getSquaredElementsAtProperty(obj, 'car');
 console.log(output); // --> [4, 1, 25]

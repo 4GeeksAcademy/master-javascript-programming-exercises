6356558242a99e82653149d4dbb2dacd06c1d5f5
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 17:37:19 +0200

    Update test.js

--- exercises/105-filterEvenLengthWords/test.js
+++ exercises/105-filterEvenLengthWords/test.js
@@ -15,22 +15,22 @@ test('Function filterEvenLengthWords must return an array', () => {
   expect(Array.isArray(filterEvenLengthWords(['some', 'text']))).toBeTruthy()
 });
 
-test('The function should return only the elements of the array whose length is an even number.', () => {
+test('The function should return only the elements of the array whose length is an even number', () => {
   let output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
   expect(output).toEqual(['word', 'word']);
 });
 
-test('The function should return only the elements of the array whose length is an even number. Testing with different values.', () => {
+test('The function should return only the elements of the array whose length is an even number. Testing with different values', () => {
   let output = filterEvenLengthWords(['tres', 'tigres', 'tristes']);
   expect(output).toEqual(['tres', 'tigres']);
 });
 
-test("If the given array didn't contains an even length word, the function should return an empty array.", () => {
+test('If the given array contains no even length words, the function should return an empty array', () => {
   let output = filterEvenLengthWords(['tre', 'tig', 't']);
   expect(output).toEqual([]);
 });
 
-test('If the array is empty, it should return an empty array.', () => {
+test('If the array is empty, it should return an empty array', () => {
   let output = filterEvenLengthWords([]);
   expect(output).toEqual([]);
 });

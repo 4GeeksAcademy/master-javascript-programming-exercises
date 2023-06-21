6aa720c3e782b3951a5757de89636ad0a8acc375
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-21 17:32:52 +0200

    Update solution.hide.js

--- exercises/105-filterEvenLengthWords/solution.hide.js
+++ exercises/105-filterEvenLengthWords/solution.hide.js
@@ -1,6 +1,6 @@
 function filterEvenLengthWords(words) {
     // your code here
-    return words.filter(word => word.length % 2 === 0);
+    return words.filter(item => item.length % 2 === 0);
 }
 
 let output = filterEvenLengthWords(['word', 'words', 'word', 'words']);

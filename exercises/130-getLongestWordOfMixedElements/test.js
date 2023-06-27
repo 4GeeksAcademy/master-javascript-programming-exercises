f6e56fc3ca10137c690adba0781170769faca9dc
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-27 22:57:01 +0200

    Update test.js

--- exercises/130-getLongestWordOfMixedElements/test.js
+++ exercises/130-getLongestWordOfMixedElements/test.js
@@ -15,17 +15,17 @@ test('Function getLongestWordOfMixedElements must return a string', () => {
   expect(typeof getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1])).toBe('string');
 });
 
-test('Function must returns the longest string in the array.', () => {
+test('Function must return the longest string in the array', () => {
   let output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
   expect(output).toBe('word');
 });
 
-test('If the given array, not contains string , it should return a empty string.', () => {
+test('If the given array contain no strings, it should return an empty string', () => {
   let output = getLongestWordOfMixedElements([1, 2, 3, 4]);
   expect(output).toBe('');
 });
 
-test('If array is empty must return a empty string', () => {
+test('If the array is empty, it must return a empty string', () => {
   let output = getLongestWordOfMixedElements([]);
   expect(output).toBe('');
 });

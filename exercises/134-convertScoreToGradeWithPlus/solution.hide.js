f4238c742d64e8f062865f9ea1e6a25eda09a8cc
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-27 23:51:59 +0200

    Update solution.hide.js

--- exercises/134-convertScoreToGradeWithPlus/solution.hide.js
+++ exercises/134-convertScoreToGradeWithPlus/solution.hide.js
@@ -1,34 +1,30 @@
 function convertScoreToGradeWithPlusAndMinus(score) {
-  // your code here
-  if (score >= 100 || score <= 0) return 'INVALID SCORE';
-  else {
-    if (score >= 90 && score <= 100) {
-      if (score >= 90 && score <= 92) return 'A-';
-      if (score >= 98 && score <= 99) return 'A+';
-      return 'A';
-    }
-    if (score >= 80 && score <= 89) {
-      if (score >= 80 && score <= 82) return 'B-';
-      if (score >= 88 && score <= 89) return 'B+';
-      return 'B';
-    }
-    if (score >= 70 && score <= 79) {
-      if (score >= 70 && score <= 72) return 'C-';
-      if (score >= 78 && score <= 79) return 'C+';
-      return 'C';
-    }
-    if (score >= 60 && score <= 69) {
-      if (score >= 60 && score <= 62) return 'D-';
-      if (score >= 68 && score <= 69) return 'D+';
-      return 'D';
-    }
-    if (score >= 0 && score <= 59) {
-      if (score >= 50 && score <= 52) return 'F-';
-      if (score >= 58 && score <= 59) return 'F+';
-      return 'F';
+    // your code here
+    if (score > 100 || score < 0) return 'INVALID SCORE';
+    else {
+      if (score >= 90 && score <= 100) {
+        if (score >= 90 && score <= 92) return 'A-';
+        if (score >= 98 && score <= 100) return 'A+';
+        return 'A';
+      }
+      if (score >= 80 && score <= 89) {
+        if (score >= 80 && score <= 82) return 'B-';
+        if (score >= 88 && score <= 89) return 'B+';
+        return 'B';
+      }
+      if (score >= 70 && score <= 79) {
+        if (score >= 70 && score <= 72) return 'C-';
+        if (score >= 78 && score <= 79) return 'C+';
+        return 'C';
+      }
+      if (score >= 60 && score <= 69) {
+        if (score >= 60 && score <= 62) return 'D-';
+        if (score >= 68 && score <= 69) return 'D+';
+        return 'D';
+      }
+      if (score >= 0 && score <= 59) return 'F';
     }
   }
-}
-
-var output = convertScoreToGradeWithPlusAndMinus(91);
+  
+let output = convertScoreToGradeWithPlusAndMinus(91);
 console.log(output); // --> 'A-'

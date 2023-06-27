f9a6b796c7f5d67042bd3bdb2a005dd26c146396
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-27 22:59:26 +0200

    Update solution.hide.js

--- exercises/130-getLongestWordOfMixedElements/solution.hide.js
+++ exercises/130-getLongestWordOfMixedElements/solution.hide.js
@@ -1,20 +1,15 @@
 function getLongestWordOfMixedElements(arr) {
-  // your code here
-  let aux = '';
-  if (arr.length > 0) {
-    let hasString = arr.find((e) => typeof e == 'string');
-    if (hasString != undefined) {
-      arr.map((e) => {
-        if (e.length > aux) aux = e;
-      });
-    } else {
-      aux = '';
+    // your code here
+    let longestWord = '';
+  
+    for (let element of arr) {
+      if (typeof element === 'string' && element.length > longestWord.length) {
+        longestWord = element;
+      }
     }
-  } else {
-    aux = '';
+  
+    return longestWord;
   }
-  return aux;
-}
 
-var output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
+let output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
 console.log(output); // --> 'word'

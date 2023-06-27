5281aca84fe852252738630bbbead41975e5d2b3
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-27 21:53:32 +0200

    Update app.js

--- exercises/127-getSumOfAllElementsAtProperty/app.js
+++ exercises/127-getSumOfAllElementsAtProperty/app.js
@@ -1,9 +1,10 @@
+function getSumOfAllElementsAtProperty(obj, key) {
+    // your code here
+    
+}
+
 let obj = {
     key: [4, 1, 8]
 };
-
-function getSumOfAllElementsAtProperty(obj, key) {
-    // your code here
-}
 let output = getSumOfAllElementsAtProperty(obj, 'key');
 console.log(output); // --> 13

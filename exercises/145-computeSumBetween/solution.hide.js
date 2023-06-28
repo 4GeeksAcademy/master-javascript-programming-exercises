ec6740713556873f84091b2db342198ef604c585
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 17:00:49 +0200

    Update solution.hide.js

--- exercises/145-computeSumBetween/solution.hide.js
+++ exercises/145-computeSumBetween/solution.hide.js
@@ -1,5 +1,5 @@
 function computeSumBetween(num1, num2) {
-    // Your code here
+    // your code here
     if (num1 > num2) return 0;
     let aux = 0;
     for (let i = num1; i < num2; i++) aux += i;

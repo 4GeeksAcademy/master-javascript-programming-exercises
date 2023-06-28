56b1d46f67293a3e05f5211526a0713257c70983
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 16:45:44 +0200

    Update app.js

--- exercises/146.2-ArrayToObject/app.js
+++ exercises/146.2-ArrayToObject/app.js
@@ -1,6 +1,7 @@
 function fromListToObject(array) {
   // your code here
+  
 }
 
 let output = fromListToObject([['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]])
-console.log(output) // => { make: 'Ford', model: 'Mustang', year: 1964 }
+console.log(output) // --> { make: 'Ford', model: 'Mustang', year: 1964 }

1bda2a70694f0995fc13f9aba5ae9729e1bbdf18
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 16:04:42 +0200

    Update test.js

--- exercises/144-multiplyBetween/test.js
+++ exercises/144-multiplyBetween/test.js
@@ -13,14 +13,14 @@ test('Function multiplyBetween must return a number', () => {
   expect(typeof multiplyBetween(1, 2)).toBe('number');
 });
 
-test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2. Testing with 2, 5.', () => {
+test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2. Testing with 2, 5', () => {
   expect(multiplyBetween(2, 5)).toBe(24);
 });
 
-test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2. Testing with 4, 8.', () => {
+test('Function must return the product multiplied of all numbers, between the two given integers, starting at num1 and excluding num2. Testing with 4, 8', () => {
   expect(multiplyBetween(4, 8)).toBe(840);
 });
 
-test('If num2 is not greater than num1, it should return 0.', () => {
+test('If num2 is not greater than num1, it should return 0', () => {
   expect(multiplyBetween(3, 2)).toBe(0);
 });

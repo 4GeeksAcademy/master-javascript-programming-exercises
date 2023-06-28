6b87d7f89eff61c47e2b66b0227191713ccd4534
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 13:52:04 +0200

    Update test.js

--- exercises/135-computeFactorialOfN/test.js
+++ exercises/135-computeFactorialOfN/test.js
@@ -13,14 +13,14 @@ test('Function computeFactorialOfN must return a number', () => {
   expect(typeof computeFactorialOfN(1)).toBe('number');
 });
 
-test('Given an integer greater than 0, returns a factorial number. Testing with 3.', () => {
+test('Given an integer greater than 0, returns a factorial number. Testing with 3', () => {
   expect(computeFactorialOfN(3)).toBe(6);
 });
 
-test('Given an integer greater than 0, returns a factorial number. Testing with 12.', () => {
+test('Given an integer greater than 0, returns a factorial number. Testing with 12', () => {
   expect(computeFactorialOfN(12)).toBe(479001600);
 });
 
-test('Given an integer greater than 0, returns a factorial number. Testing with 4.', () => {
+test('Given an integer greater than 0, returns a factorial number. Testing with 4', () => {
   expect(computeFactorialOfN(4)).toBe(24);
 });

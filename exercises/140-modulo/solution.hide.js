81900a6174dfb179c17e2ca67a625c460d6576b4
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 15:21:25 +0200

    Update solution.hide.js

--- exercises/140-modulo/solution.hide.js
+++ exercises/140-modulo/solution.hide.js
@@ -1,7 +1,7 @@
 function modulo(num1, num2) {
   // your code here
   let i = 0;
-  if(num1 > 0){
+  if(num1 > 0) {
       while (i < num1) {
           if(i+num2 > num1) break;
           i += num2

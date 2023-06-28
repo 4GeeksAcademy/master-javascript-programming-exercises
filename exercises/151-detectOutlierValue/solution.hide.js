dd127e06ab54096e0b0c648cde5865f58b8f9084
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 20:29:35 +0200

    Update solution.hide.js

--- exercises/151-detectOutlierValue/solution.hide.js
+++ exercises/151-detectOutlierValue/solution.hide.js
@@ -19,7 +19,7 @@ function detectOutlierValue(string) {
 }
 
 // Third number is odd, while the rest of the numbers are even
-console.log(detectOutlierValue('2 4 7 8 10')); // => 3
+console.log(detectOutlierValue('2 4 7 8 10')); // --> 3
 
 // Second number is even, while the rest of the numbers are odd
-console.log(detectOutlierValue('1 10 1 1')); // => 2
+console.log(detectOutlierValue('1 10 1 1')); // --> 2

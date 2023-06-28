0704b8aabae58136bce5304649173379fcba5be7
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-06-28 15:19:11 +0200

    Update test.js

--- exercises/140-modulo/test.js
+++ exercises/140-modulo/test.js
@@ -13,22 +13,22 @@ test('Function modulo must return a number', () => {
   expect(typeof modulo(1, 2)).toBe('number');
 });
 
-test('Given 2 numbers, return the remainder after dividing num1 by num2. Testing with (5, 55).', () => {
+test('Given 2 numbers, return the remainder after dividing num1 by num2. Testing with (5, 55)', () => {
   expect(modulo(5, 55)).toBe(5);
 });
 
-test('Given 2 numbers, return the remainder after dividing num1 by num2.Testing with (25, 4).', () => {
+test('Given 2 numbers, return the remainder after dividing num1 by num2. Testing with (25, 4)', () => {
   expect(modulo(25, 4)).toBe(1);
 });
 
-test('The module always returns the sign of the first number. Testing with (-5, 55).', () => {
+test('The module always returns the sign of the first number. Testing with (-5, 55)', () => {
   expect(modulo(-5, 55)).toBe(-5);
 });
 
-test('The module always returns the sign of the first number. Testing with (-25, 4).', () => {
+test('The module always returns the sign of the first number. Testing with (-25, 4)', () => {
   expect(modulo(-25, 4)).toBe(-1);
 });
 
-test('If any of the operators is different from a number, then the result is NaN.', () => {
+test('If any of the operators is different from a number, then the result is NaN', () => {
   expect(modulo(25, 'hey')).toBe(NaN);
 });

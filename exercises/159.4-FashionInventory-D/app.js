38afed3f0bb106d7718b2735b869f519b01da0aa
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-05 01:49:09 +0200

    Update app.js

--- exercises/159.4-FashionInventory-D/app.js
+++ exercises/159.4-FashionInventory-D/app.js
@@ -5,16 +5,16 @@ let currentInventory = [
       { name: 'tasselled black low-top lace-up', price: 1000 },
       { name: 'tasselled green low-top lace-up', price: 1100 },
       { name: 'plain beige suede moccasin', price: 950 },
-      { name: 'plain olive suede moccasin', price: 1050 },
-    ],
+      { name: 'plain olive suede moccasin', price: 1050 }
+    ]
   },
   {
     name: 'Gucci',
     shoes: [
       { name: 'red leather laced sneakers', price: 800 },
-      { name: 'black leather laced sneakers', price: 900 },
-    ],
-  },
+      { name: 'black leather laced sneakers', price: 900 }
+    ]
+  }
 ];
 
 function getLaceNameDataForShoes(inventory) {

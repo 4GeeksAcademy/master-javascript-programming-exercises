4967f10ea63225bb897eae7ecafa64127c54bb40
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-05 15:47:32 +0200

    Update solution.hide.js

--- exercises/060-removeFromFrontOfNew/solution.hide.js
+++ exercises/060-removeFromFrontOfNew/solution.hide.js
@@ -1,7 +1,6 @@
 function removeFromFrontOfNew(arr) {
     // your code here
-    let newArr = arr.filter((el, index) => index != 0) // creates new array with all elements of arr but the first one
-    return newArr
+    return arr.slice(1);
 }
 
 let arr = [1, 2, 3];

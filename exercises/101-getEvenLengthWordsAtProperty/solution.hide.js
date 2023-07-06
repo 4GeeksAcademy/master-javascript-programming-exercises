edd4ef1aab5a55d92e9f086074185dc6a481b2ff
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-06 21:32:25 +0200

    Update solution.hide.js

--- exercises/101-getEvenLengthWordsAtProperty/solution.hide.js
+++ exercises/101-getEvenLengthWordsAtProperty/solution.hide.js
@@ -6,3 +6,9 @@ function getEvenLengthWordsAtProperty(obj, key) {
   
   return obj[key].filter(word => word.length % 2 === 0);
 }
+
+let obj = {
+  key: ['a', 'long', 'game']
+};
+let output = getEvenLengthWordsAtProperty(obj, 'key');
+console.log(output); // --> ['long', 'game']

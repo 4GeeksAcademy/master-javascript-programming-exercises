61ba7c8bbe8c181476083c6eb665426cac882616
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-06 18:35:26 +0200

    Update solution.hide.js

--- exercises/083-getAllElementsButNth/solution.hide.js
+++ exercises/083-getAllElementsButNth/solution.hide.js
@@ -3,3 +3,6 @@ function getAllElementsButNth(array, n) {
     array.splice(n, 1);
     return array
 }
+
+let output = getAllElementsButNth(['a', 'b', 'c'], 1);
+console.log(output); // --> ['a', 'c']

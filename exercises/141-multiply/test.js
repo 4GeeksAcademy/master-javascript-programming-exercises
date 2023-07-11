dac29b4f8d0c226369b485cf6f8aae613c42976b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-11 21:38:28 +0200

    Update test.js

--- exercises/141-multiply/test.js
+++ exercises/141-multiply/test.js
@@ -13,19 +13,19 @@ test('Function multiply must return a number', () => {
   expect(typeof multiply(1, 2)).toBe('number');
 });
 
-test('Given 2 whole numbers, multiply and return the total', () => {
+test('Given 2 integer numbers, multiply and return the total', () => {
   expect(multiply(4, 7)).toBe(28);
 });
 
-test('Given 2 whole numbers, multiply and return the total. Testing with different values', () => {
+test('Given 2 integer numbers, multiply and return the total. Testing with different values', () => {
   expect(multiply(5, -5)).toBe(-25);
 });
 
-test('Given 2 whole numbers, multiply and return the total. Testing with different values', () => {
+test('Given 2 integer numbers, multiply and return the total. Testing with different values', () => {
   expect(multiply(-5, -3)).toBe(15);
 });
 
-test('Given 2 whole numbers, multiply and return the total. Testing with different values', () => {
+test('Given 2 integer numbers, multiply and return the total. Testing with different values', () => {
   expect(multiply(0, -5)).toBe(0);
 });
 

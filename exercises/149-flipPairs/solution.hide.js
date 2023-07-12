c9053514a1426a9eab27bcf3437025cbe0342196
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-12 05:21:48 +0200

    Update solution.hide.js

--- exercises/149-flipPairs/solution.hide.js
+++ exercises/149-flipPairs/solution.hide.js
@@ -4,7 +4,6 @@ function flipPairs(input) {
     return result;
   }
 
-
 let input = "Can you see what this is about?";
 let output = flipPairs(input);
 console.log(output); // --> aC noy ues ehwtat ih ssia obtu?

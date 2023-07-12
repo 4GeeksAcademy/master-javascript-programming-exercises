27f64bff4e33e46e14ee549a3bfb4b38d53c68f2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-12 05:14:21 +0200

    Update solution.hide.js

--- exercises/149-flipPairs/solution.hide.js
+++ exercises/149-flipPairs/solution.hide.js
@@ -1,9 +1,29 @@
 function flipPairs(input) {
-    // your code here
+  // your code here
+  let flippedString = '';
+
+  for (let i = 0; i < input.length; i += 2) {
+    if (i + 1 < input.length) {
+      flippedString += input[i + 1] + input[i];
+    } else {
+      flippedString += input[i];
+    }
+  }
+
+  return flippedString;
+}
+
+let input = "Can you see what this is about?";
+let output = flipPairs(input);
+console.log(output); // --> aC noy ues ehwtat ih ssia obtu?  
+
+/*  // Solution 2:
+
+function flipPairs(input) {
+    // This solution is using Regular Expressions or RegExp
     let result = input.replace(/(.)(.)/g, '$2$1');
     return result;
   }
-  
-let input = "Can you see what this is about?";
-let output = flipPairs(input);
-console.log(output); // --> ?tuoba si siht tahw ees uoy naC
+
+*/
+

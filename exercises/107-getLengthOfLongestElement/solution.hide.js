efbb1f44b6db5f5e5346938b5398d95650ab05ef
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2023-07-12 17:01:54 -0400

    Update solution.hide.js

--- exercises/107-getLengthOfLongestElement/solution.hide.js
+++ exercises/107-getLengthOfLongestElement/solution.hide.js
@@ -1,9 +1,5 @@
 function getLengthOfLongestElement(arr) {
   // your code here
-  if (arr.length === 0) {
-    return 0;
-  }
-
   let longestLength = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].length > longestLength) {

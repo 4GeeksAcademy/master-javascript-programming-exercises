c37e9bff3101eb9d2df4a4ea57dfb1158c7e0fc6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-07-12 04:12:37 +0200

    Update app.js

--- exercises/146.3-ArrayToObject/app.js
+++ exercises/146.3-ArrayToObject/app.js
@@ -2,20 +2,3 @@ function transformEmployeeData(array) {
   // your code here
   
 }
-
-let output = transformEmployeeData([
-  [
-    ['firstName', 'Joe'],
-    ['lastName', 'Blow'],
-    ['age', 42],
-    ['role', 'clerk'],
-  ],
-  [
-    ['firstName', 'Mary'],
-    ['lastName', 'Jenkins'],
-    ['age', 36],
-    ['role', 'manager'],
-  ],
-]);
-
-console.log(output);

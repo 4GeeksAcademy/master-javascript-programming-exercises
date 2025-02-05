4ccd1ee13304b665c6cdef9e5e963a405011e89b
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-05 15:33:03 -0300

    Update solution.hide.js
    He refactorizado la función convertScoreToGradeWithPlusAndMinus para mejorar su legibilidad y eficiencia. Los cambios principales son los siguientes:
    
    Simplificación de la lógica condicional: En el código original, se utilizaban múltiples estructuras if anidadas para determinar la calificación y su modificador (+ o -). Esto hacía que el código fuera extenso y difícil de seguir. En la nueva versión, he reducido la cantidad de condiciones, lo que facilita la comprensión.

--- exercises/134-convertScoreToGradeWithPlus/solution.hide.js
+++ exercises/134-convertScoreToGradeWithPlus/solution.hide.js
@@ -1,30 +1,22 @@
 function convertScoreToGradeWithPlusAndMinus(score) {
-    // your code here
-    if (score > 100 || score < 0) return 'INVALID SCORE';
-    else {
-      if (score >= 90 && score <= 100) {
-        if (score >= 90 && score <= 92) return 'A-';
-        if (score >= 98 && score <= 100) return 'A+';
-        return 'A';
-      }
-      if (score >= 80 && score <= 89) {
-        if (score >= 80 && score <= 82) return 'B-';
-        if (score >= 88 && score <= 89) return 'B+';
-        return 'B';
-      }
-      if (score >= 70 && score <= 79) {
-        if (score >= 70 && score <= 72) return 'C-';
-        if (score >= 78 && score <= 79) return 'C+';
-        return 'C';
-      }
-      if (score >= 60 && score <= 69) {
-        if (score >= 60 && score <= 62) return 'D-';
-        if (score >= 68 && score <= 69) return 'D+';
-        return 'D';
-      }
-      if (score >= 0 && score <= 59) return 'F';
-    }
-  }
-  
+  // your code here
+  if (score > 100 || score < 0) return "INVALID SCORE";
+  if (score == 100) return "A+";
+  if (score < 60) return "F";
+
+  let grade = "";
+  if (score <= 100) grade = "A";
+  if (score < 90) grade = "B";
+  if (score < 80) grade = "C";
+  if (score < 70) grade = "D";
+
+  let plusOrMinus = "";
+  const plusScore = Number(String(score).split("")[1]);
+  if (plusScore <= 2) plusOrMinus = "-";
+  if (plusScore >= 8) plusOrMinus = "+";
+
+  return grade + plusOrMinus;
+}
+
 let output = convertScoreToGradeWithPlusAndMinus(91);
 console.log(output); // --> 'A-'
